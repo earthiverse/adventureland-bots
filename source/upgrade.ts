0d5e2130dd980210aec1e2cc4639094d5b23a49c
Kent Rasmussen <hyprkookeez@gmail.com>
2019-12-09 17:59:22 +0900

    Add compound if many function

--- source/upgrade.ts
+++ source/upgrade.ts
@@ -1,4 +1,4 @@
-import { determineGrade, findItems, findItemsWithLevel } from "./functions";
+import { determineGrade, findItems, findItemsWithLevel, getInventory } from "./functions";
 import { ItemName } from "./definitions/adventureland";
 
 export function compoundItem(itemname: ItemName, target_level: number) {
@@ -29,7 +29,7 @@ export function compoundItem(itemname: ItemName, target_level: number) {
         // Didn't find enough items to combine...
         return;
 
-    let grade = determineGrade(items[0][1]);
+    let grade = determineGrade(itemname, item_level);
     let scrolls;
     if (grade == 0) {
         scrolls = findItems("cscroll0");
@@ -63,7 +63,7 @@ export function upgradeItem(itemname: ItemName, target_level: number) {
 
     for (let [i, item] of items) {
         if (item.level < target_level) {
-            let grade = determineGrade(item);
+            let grade = determineGrade(itemname, item.level);
             let scrolls;
             if (grade == 0) {
                 scrolls = findItems("scroll0");
@@ -78,7 +78,49 @@ export function upgradeItem(itemname: ItemName, target_level: number) {
     }
 }
 
-// TODO: CompoundIfMany function
+export function compoundIfMany(maxLevel: number) {
+    let foundUpgrade = false;
+    for (let npc of parent.npcs) {
+        if (npc.id == "newupgrade" && distance(character, {
+            x: npc.position[0],
+            y: npc.position[1]
+        }) < 350) {
+            foundUpgrade = true;
+            break;
+        }
+    }
+    if (!foundUpgrade) return; // Can't upgrade, nobody is near.
+    if (parent.character.q && parent.character.q["compound"]) return; // Already compounding
+
+    // name, level, slot #
+    let items: [ItemName, number, number][] = [];
+    getInventory().forEach((item) => {
+        if (!G.items[item[1].name].compound) return; // We can't compound this item
+        if (item[1].level >= maxLevel) return; // The item level's higher than we want to upgrade
+        items.push([item[1].name, item[1].level, item[0]])
+    })
+    items.sort();
+
+    for (let i = 0; i < items.length - 2; i++) {
+        if(items[i][0] == items[i+1][0] && items[i][0] == items[i+2][0] // Match names
+            && items[i][1] == items[i+1][1] && items[i][1] == items[i+2][1] // Match levels
+            ) {
+                // Found 3 identical items to compound
+                let grade = determineGrade(items[i][0], items[i][1]);
+                let scrolls;
+                if (grade == 0) {
+                    scrolls = findItems("cscroll0");
+                } else if (grade == 1) {
+                    scrolls = findItems("cscroll1");
+                } else if (grade == 2) {
+                    scrolls = findItems("cscroll2");
+                }
+                compound(items[i][2], items[i+1][2], items[i+2][2], scrolls[0][0])
+                return;
+        }
+
+    }
+}
 
 /**
  * This function will upgrade items in your inventory if there are more than one of the same item. It will upgrade until one breaks, keeping the higher level item, and upgrading the lower level item.
@@ -105,10 +147,9 @@ export function upgradeIfMany(maxLevel: number) {
 
         let minimumLevel = Math.min(...items.map(([, { level }]) => level)) // Find the minimum level
         items = findItemsWithLevel(parent.character.items[i].name, minimumLevel);
-        let level = items[0][1].level;
-        if (level >= maxLevel) continue; // Don't upgrade high level items
+        if (items[0][1].level >= maxLevel) continue; // Don't upgrade high level items
 
-        let grade = determineGrade(items[0][1]);
+        let grade = determineGrade(items[0][1].name, items[0][1].level);
         let scrolls;
         if (grade == 0) {
             scrolls = findItems("scroll0");
