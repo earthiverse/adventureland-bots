aef87d9bcf363f3b36779e85b1a38980fbc237f3
Kent Rasmussen <hyprkookeez@gmail.com>
2020-01-25 10:43:01 +0900

    Remove overrides, add monster hunt progress

--- source/saves/merchant.js
+++ source/saves/merchant.js
@@ -5,6 +5,21 @@
 import("http://localhost:3000/merchant.js")
     .then(() => {
         bots.merchant.run()
+
+        // Show Quest Info Periodically
+        // TODO: Make a GUI element that shows this information instead
+        function showQuests() {
+            for(const id in bots.merchant.info.party) {
+                if(!parent.party_list.includes(id)) continue
+                const member = bots.merchant.info.party[id]
+                if(member.s.monsterhunt) {
+                    game_log(member.s.monsterhunt.id + ": " + member.s.monsterhunt.c + " in " + Math.floor(member.s.monsterhunt.ms / 1000 / 60) + "m")
+                }
+            }
+            game_log("Coins: " + parent.character.items[30].q)
+            setTimeout(() => { showQuests() }, 10000)
+        }
+        showQuests()
     }, () => {
         load_code("merchant")
         bots.merchant.run()
