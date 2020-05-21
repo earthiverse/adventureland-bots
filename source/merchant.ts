bc181d1250d4fc8c07f63c67d8b79d63432fabae
Kent Rasmussen <hyprkookeez@gmail.com>
2020-05-21 14:35:07 +0900

    Maybe fix stackable items not being put in bank?

--- source/merchant.ts
+++ source/merchant.ts
@@ -244,7 +244,6 @@ class Merchant extends Character {
         for (const item of getInventory()) {
             if (emptyBankSlots.length == 0) break
             if (this.itemsToKeep.includes(item.name)) continue
-            if (item.q) continue // We'll deal with stackable items later
 
             emptyBankSlots.shift()
             bank_store(item.index)
