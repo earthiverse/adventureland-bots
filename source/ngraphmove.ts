f10830e7843bc051ca88329b1ff3689bd9c42c26
Kent Rasmussen <hyprkookeez@gmail.com>
2020-08-23 08:10:45 +0900

    Debug pathing

--- source/ngraphmove.ts
+++ source/ngraphmove.ts
@@ -716,6 +716,9 @@ export class NGraphMove {
             if ((!linkData && !can_move_to(toData.x, toData.y)) || parent.character.map !== fromData.map) {
                 // We got lost somewhere, retry
                 console.warn("NGraphMove movement failed. We're going to teleport to town and try again.")
+                console.log("----- Path -----")
+                console.log(path)
+                console.log("----------------")
                 use_skill("town")
                 await new Promise(resolve => setTimeout(resolve, Math.max(...parent.pings) + TOWN_TIME))
                 return this.move(goal, finishDistanceTolerance)
