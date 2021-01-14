570e13d6c06b90ed8f2194c1d15fb368c8ce4b6a
Kent Rasmussen <hyprkookeez@gmail.com>
2021-01-14 11:29:23 +0900

    Switch to using rangers

--- source/bscorpion_farmer.ts
+++ source/bscorpion_farmer.ts
@@ -2,16 +2,16 @@ import AL from "alclient"
 
 /** Config */
 const merchantName = "earthMer"
-const priestName = "earthPri"
-const mageName = "earthMag"
-const rangerName = "earthiverse"
+const ranger1Name = "earthiverse"
+const ranger2Name = "earthRan2"
+const ranger3Name = "earthRan3"
 const region: AL.ServerRegion = "ASIA"
 const identifier: AL.ServerIdentifier = "I"
 
 let merchant: AL.Merchant
-let priest: AL.Priest
-let mage: AL.Mage
-let ranger: AL.Ranger
+let ranger1: AL.Ranger
+let ranger2: AL.Ranger
+let ranger3: AL.Ranger
 
 async function startRanger(ranger: AL.Ranger) {
     async function attackLoop() {
@@ -36,18 +36,24 @@ async function startRanger(ranger: AL.Ranger) {
                 const spawnAngle = Math.atan2(bscorpionSpawn.y - ranger.y, bscorpionSpawn.x - ranger.x)
                 forces.push({ strength: spawnStrength, angle: spawnAngle })
 
-                // Force #2: Perpendicular to the center of the bscorpion spawn (so we move in a circle)
-                const circleStrength = ranger.speed / 4
-                const circleAngle = spawnAngle + Math.PI / 2
-                forces.push({ strength: circleStrength, angle: circleAngle })
-
-                // Force #3: Away from the bscorpion
                 const nearest = ranger.getNearestMonster("bscorpion")
                 if (nearest) {
                     const bscorpion = nearest.monster
-                    if (bscorpion && bscorpion.isAttackingUs(ranger)) {
-                        const bscorpionStrength = nearest.distance - bscorpion.range
+                    if (AL.Tools.distance(ranger, bscorpion) < bscorpion.range * 3) {
+                        // Force #2: Away from the bscorpion
+                        const bscorpionStrength = - Math.max(bscorpion.range * 3 - nearest.distance, ranger.range - bscorpion.speed)
                         const bscorpionAngle = Math.atan2(bscorpion.y - ranger.y, bscorpion.x - ranger.x)
+                        forces.push({ strength: bscorpionStrength, angle: bscorpionAngle })
+
+                        // Force #3: Perpendicular to the center of the bscorpion spawn (so we move in a circle) away from the bscorpion
+                        const circleStrength = ranger.speed / 4
+                        let circleAngle
+                        if (bscorpionAngle > spawnAngle) {
+                            circleAngle = spawnAngle - Math.PI / 2
+                        } else {
+                            circleAngle = spawnAngle + Math.PI / 2
+                        }
+                        forces.push({ strength: circleStrength, angle: circleAngle })
                     }
                 }
 
@@ -59,6 +65,7 @@ async function startRanger(ranger: AL.Ranger) {
                     newY += Math.sin(force.angle) * force.strength
                 }
 
+                // TODO: Confirm that we need the empty function
                 // We want to continuously move, so we don't mind the empty function
                 // eslint-disable-next-line @typescript-eslint/no-empty-function
                 ranger.move(newX, newY).catch(() => { })
@@ -67,32 +74,6 @@ async function startRanger(ranger: AL.Ranger) {
                 // Move to the bscorpion spawn
                 await ranger.smartMove(bscorpionSpawn, { getWithin: ranger.range / 2 })
             }
-        } catch (e) {
-            console.error(e)
-        }
-
-        setTimeout(async () => { moveLoop() }, 250)
-    }
-    moveLoop()
-}
-
-async function startPriest(priest: AL.Priest) {
-    async function moveLoop() {
-        try {
-            await priest.smartMove("main")
-        } catch (e) {
-            console.error(e)
-        }
-
-        setTimeout(async () => { moveLoop() }, 250)
-    }
-    moveLoop()
-}
-
-async function startMage(mage: AL.Mage) {
-    async function moveLoop() {
-        try {
-            await mage.smartMove("main")
         } catch (e) {
             console.error(e)
         }
@@ -122,13 +103,13 @@ async function run() {
     // Start all characters
     console.log("Connecting...")
     const merchantP = AL.Game.startMerchant(merchantName, region, identifier)
-    const priestP = AL.Game.startPriest(priestName, region, identifier)
-    const mageP = AL.Game.startMage(mageName, region, identifier)
-    const rangerP = AL.Game.startRanger(rangerName, region, identifier)
+    const ranger1P = AL.Game.startRanger(ranger1Name, region, identifier)
+    const ranger2P = AL.Game.startRanger(ranger2Name, region, identifier)
+    const ranger3P = AL.Game.startRanger(ranger3Name, region, identifier)
     merchant = await merchantP
-    priest = await priestP
-    mage = await mageP
-    ranger = await rangerP
+    ranger1 = await ranger1P
+    ranger2 = await ranger2P
+    ranger3 = await ranger3P
 
     // Set up functionality to reconnect if we disconnect
     // TODO: Add a delay
@@ -139,14 +120,14 @@ async function run() {
         character.socket.on("disconnect", async () => { await reconnect(character) })
     }
     merchant.socket.on("disconnect", async () => { await reconnect(merchant) })
-    priest.socket.on("disconnect", async () => { await reconnect(priest) })
-    mage.socket.on("disconnect", async () => { await reconnect(mage) })
-    ranger.socket.on("disconnect", async () => { await reconnect(ranger) })
+    ranger1.socket.on("disconnect", async () => { await reconnect(ranger1) })
+    ranger2.socket.on("disconnect", async () => { await reconnect(ranger2) })
+    ranger3.socket.on("disconnect", async () => { await reconnect(ranger3) })
 
     // Start the characters
     startMerchant(merchant)
-    startPriest(priest)
-    startMage(mage)
-    startRanger(ranger)
+    startRanger(ranger1)
+    startRanger(ranger2)
+    startRanger(ranger3)
 }
 run()
