9aa33e15fe8c43fd3eba0229b0132862bff961f3
Kent Rasmussen <hyprkookeez@gmail.com>
2021-01-14 18:40:29 +0900

    I made a script to set my mail to 0.

---
+++ source/mailGrabber.ts
@@ -0,0 +1,14 @@
+import AL from "alclient"
+import fs from "fs"
+
+AL.Game.loginJSONFile("../credentials.json").then(async () => {
+    console.log("Grabbing all mail...")
+    const mail = await AL.Game.getMail(true)
+    console.log(`  All mail retrieved! Saving ${mail.length} messages to mail.json.`)
+    fs.writeFileSync("mail.json", JSON.stringify(mail))
+
+    // Mark all messages as read
+    for (const message of mail) {
+        console.log(await AL.Game.markMailAsRead(message.id))
+    }
+})
