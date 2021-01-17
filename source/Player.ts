6eade8671022db3782d7ce99db697d7cf4019f09
Kent Rasmussen <hyprkookeez@gmail.com>
2021-01-17 13:51:58 +0900

    debug weird positioning

--- source/Player.ts
+++ source/Player.ts
@@ -64,18 +64,18 @@ export class Player extends Observer {
             this.chests.delete(data.id)
         })
 
-        // TODO: Create a CorrectionData object
-        this.socket.on("correction", (data: { x: number, y: number }) => {
-            console.log("----- LOCATION CORRECTION -----")
-            console.log(data)
+        // // TODO: Create a CorrectionData object
+        // this.socket.on("correction", (data: { x: number, y: number }) => {
+        //     console.log("----- LOCATION CORRECTION -----")
+        //     console.log(data)
 
-            this.updatePositions()
-            this.character.x = data.x
-            this.character.y = data.y
-            this.character.going_x = data.x
-            this.character.going_y = data.y
-            this.character.moving = false
-        })
+        //     this.updatePositions()
+        //     this.character.x = data.x
+        //     this.character.y = data.y
+        //     this.character.going_x = data.x
+        //     this.character.going_y = data.y
+        //     this.character.moving = false
+        // })
 
         this.socket.on("death", (data: DeathData) => {
             const entity = this.entities.get(data.id)
