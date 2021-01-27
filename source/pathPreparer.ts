990882c55110dd4409008255a2f81ab542020398
Kent Rasmussen <hyprkookeez@gmail.com>
2021-01-27 10:12:24 +0900

    Add a benchmark for pathfinding

---
+++ source/pathPreparer.ts
@@ -0,0 +1,15 @@
+import AL from "alclient"
+AL.Game.loginJSONFile("../credentials.json").then(async () => {
+    console.log("Preparing pathfinding...")
+    await AL.Pathfinder.prepare()
+
+    console.log("Finding path from 'main' to 'spookytown'")
+    let start = Date.now()
+    let path = AL.Pathfinder.getPath({ map: "main", x: 0, y: 0 }, { map: "spookytown", x: 0, y: 0 })
+    console.log(`Computed path in ${Date.now() - start}ms`)
+
+    console.log("Finding path from 'spookytown' to 'main'")
+    start = Date.now()
+    path = AL.Pathfinder.getPath({ map: "spookytown", x: 0, y: 0 }, { map: "main", x: 0, y: 0 })
+    console.log(`Computed path in ${Date.now() - start}ms`)
+})
