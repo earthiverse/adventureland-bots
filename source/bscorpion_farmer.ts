d584a335e0b826a9b9b257e7fbfc93976805f1f1
Kent Rasmussen <hyprkookeez@gmail.com>
2021-04-18 21:25:11 +0900

    mluck and bank and stuff

--- source/bscorpion_farmer.ts
+++ source/bscorpion_farmer.ts
@@ -1,4 +1,5 @@
 import AL from "alclient"
+import { NodeData } from "alclient/build/definitions/pathfinder"
 
 /** Config */
 const merchantName = "earthMer"
@@ -15,6 +16,29 @@ let rogue: AL.Rogue
 
 const RADIUS = 125
 const MOVE_TIME_MS = 500
+const MERCHANT_GOLD_TO_HOLD = 100_000_000
+const MERCHANT_ITEMS_TO_HOLD: AL.ItemName[] = [
+    // Things we keep on ourselves
+    "computer", "tracker", "stand0", "xptome",
+    // Boosters
+    "luckbooster", "goldbooster", "xpbooster",
+    // Potions
+    "hpot0", "hpot1", "mpot0", "mpot1",
+    // MH Tokens
+    "monstertoken",
+    // Scrolls
+    "cscroll0", "cscroll1", "cscroll2", "cscroll3", "scroll0", "scroll1", "scroll2", "scroll3", "scroll4", "strscroll", "intscroll", "dexscroll",
+    // Fishing Rod
+    "rod",
+    // Main Items
+    "dartgun", "wbook1",
+
+    // TEMP: For crafting pouchbows
+    "smoke",
+
+    // TEMP: For crafting eggbaskets
+    "egg0", "egg1", "egg2", "egg3", "egg4", "egg5", "egg6", "egg7", "egg8"
+]
 
 async function startShared(bot: AL.Character) {
     async function buyLoop() {
@@ -408,72 +432,252 @@ async function startMerchant(merchant: AL.Merchant) {
         }
     })
 
-    async function healLoop() {
+    async function mluckLoop() {
         try {
-            if (merchant.socket.disconnected) {
-                setTimeout(async () => { healLoop() }, 10)
-                return
-            }
+            if (merchant.socket.disconnected) return
 
-            if (!merchant.rip) {
-                const missingHP = merchant.max_hp - merchant.hp
-                const missingMP = merchant.max_mp - merchant.mp
-                const hpRatio = merchant.hp / merchant.max_hp
-                const mpRatio = merchant.mp / merchant.max_mp
-                const hpot1 = merchant.locateItem("hpot1")
-                const hpot0 = merchant.locateItem("hpot0")
-                const mpot1 = merchant.locateItem("mpot1")
-                const mpot0 = merchant.locateItem("mpot0")
-                if (hpRatio < mpRatio) {
-                    if (merchant.c.town) {
-                        await merchant.regenHP()
-                    } else if (missingHP >= 400 && hpot1 !== undefined) {
-                        await merchant.useHPPot(hpot1)
-                    } else if (missingHP >= 200 && hpot0 !== undefined) {
-                        await merchant.useHPPot(hpot0)
-                    } else {
-                        await merchant.regenHP()
-                    }
-                } else if (mpRatio < hpRatio) {
-                    if (merchant.c.town) {
-                        await merchant.regenHP()
-                    } else if (missingMP >= 500 && mpot1 !== undefined) {
-                        await merchant.useMPPot(mpot1)
-                    } else if (missingMP >= 300 && mpot0 !== undefined) {
-                        await merchant.useMPPot(mpot0)
-                    } else {
-                        await merchant.regenMP()
-                    }
-                } else if (hpRatio < 1) {
-                    if (merchant.c.town) {
-                        await merchant.regenHP()
-                    } else if (missingHP >= 400 && hpot1 !== undefined) {
-                        await merchant.useHPPot(hpot1)
-                    } else if (missingHP >= 200 && hpot0 !== undefined) {
-                        await merchant.useHPPot(hpot0)
-                    } else {
-                        await merchant.regenHP()
+            if (merchant.canUse("mluck")) {
+                if (!merchant.s.mluck || merchant.s.mluck.f !== merchant.id) await merchant.mluck(merchant.id) // mluck ourselves
+
+                for (const [, player] of merchant.players) {
+                    if (AL.Tools.distance(merchant, player) > merchant.G.skills.mluck.range) continue // Too far away to mluck
+                    if (player.npc) continue // It's an NPC, we can't mluck NPCs.
+
+                    if (!player.s.mluck) {
+                        await merchant.mluck(player.id) // Give the mluck 
+                    } else if (!player.s.mluck.strong && player.s.mluck.f !== merchant.id) {
+                        await merchant.mluck(player.id) // Steal the mluck
+                    } else if ((!player.s.mluck.strong && player.s.mluck.ms < (merchant.G.conditions.mluck.duration - 60000))
+                        || (player.s.mluck.strong && player.s.mluck.f == merchant.id && player.s.mluck.ms < (merchant.G.conditions.mluck.duration - 60000))) {
+                        await merchant.mluck(player.id) // Extend the mluck
                     }
                 }
             }
-
         } catch (e) {
             console.error(e)
         }
 
-        setTimeout(async () => { healLoop() }, Math.max(merchant.getCooldown("use_hp"), 10))
+        setTimeout(async () => { mluckLoop() }, 250)
     }
-    healLoop()
+    mluckLoop()
 
+    let lastBankVisit = Number.MIN_VALUE
     async function moveLoop() {
         try {
+            if (merchant.socket.disconnected) return
+
+            // If we are dead, respawn
+            if (merchant.rip) {
+                await merchant.respawn()
+                setTimeout(async () => { moveLoop() }, 1000)
+                return
+            }
+
+            // If we are full, let's go to the bank
+            let freeSlots = 0
+            for (const item of merchant.items) {
+                if (!item) freeSlots++
+            }
+            if (freeSlots == 0 || lastBankVisit < Date.now() - 120000 || merchant.hasPvPMarkedItem()) {
+                await merchant.closeMerchantStand()
+                await merchant.smartMove("items1")
+
+                lastBankVisit = Date.now()
+
+                // Deposit excess gold
+                const excessGold = merchant.gold - MERCHANT_GOLD_TO_HOLD
+                if (excessGold > 0) {
+                    await merchant.depositGold(excessGold)
+                } else if (excessGold < 0) {
+                    await merchant.withdrawGold(-excessGold)
+                }
+
+                // Deposit items
+                for (let i = 0; i < merchant.items.length; i++) {
+                    const item = merchant.items[i]
+                    if (!item) continue
+                    if (!MERCHANT_ITEMS_TO_HOLD.includes(item.name) /* We don't want to hold on to it */
+                        || item.v) /* Item is PvP marked */ {
+                        // Deposit it in the bank
+                        try {
+                            await merchant.depositItem(i)
+                        } catch (e) {
+                            console.error(e)
+                        }
+                    }
+                }
+
+                // Store information about everything in our bank to use it later to find upgradable stuff
+                const bankItems: AL.ItemInfo[] = []
+                for (let i = 0; i <= 7; i++) {
+                    const bankPack = `items${i}` as Exclude<AL.BankPackName, "gold">
+                    for (const item of merchant.bank[bankPack]) {
+                        bankItems.push(item)
+                    }
+                }
+                let freeSpaces = merchant.esize
+                const duplicates = merchant.locateDuplicateItems(bankItems)
+
+                // Withdraw compoundable & upgradable things
+                for (const iN in duplicates) {
+                    const itemName = iN as AL.ItemName
+                    const d = duplicates[itemName]
+                    const gInfo = merchant.G.items[itemName]
+                    if (gInfo.upgrade) {
+                        // Withdraw upgradable items
+                        if (freeSpaces < 3) break // Not enough space in inventory
+
+                        const pack1 = `items${Math.floor((d[0]) / 42)}` as Exclude<AL.BankPackName, "gold">
+                        const slot1 = d[0] % 42
+                        let withdrew = false
+                        for (let i = 1; i < d.length && freeSpaces > 2; i++) {
+                            const pack2 = `items${Math.floor((d[i]) / 42)}` as Exclude<AL.BankPackName, "gold">
+                            const slot2 = d[i] % 42
+                            const item2 = merchant.bank[pack2][slot2]
+                            const level0Grade = gInfo.grades.lastIndexOf(0) + 1
+
+                            if (item2.level >= 9 - level0Grade) continue // We don't want to upgrade high level items automatically
+
+                            try {
+                                await merchant.withdrawItem(pack2, slot2)
+                                withdrew = true
+                                freeSpaces--
+                            } catch (e) {
+                                console.error(e)
+                            }
+                        }
+                        if (withdrew) {
+                            try {
+                                await merchant.withdrawItem(pack1, slot1)
+                                freeSpaces--
+                            } catch (e) {
+                                console.error(e)
+                            }
+                        }
+                    } else if (gInfo.compound) {
+                        // Withdraw compoundable items
+                        if (freeSpaces < 5) break // Not enough space in inventory
+                        if (d.length < 3) continue // Not enough to compound
+
+                        for (let i = 0; i < d.length - 2 && freeSpaces > 4; i++) {
+                            const pack1 = `items${Math.floor((d[i]) / 42)}` as Exclude<AL.BankPackName, "gold">
+                            const slot1 = d[i] % 42
+                            const item1 = merchant.bank[pack1][slot1]
+                            const pack2 = `items${Math.floor((d[i + 1]) / 42)}` as Exclude<AL.BankPackName, "gold">
+                            const slot2 = d[i + 1] % 42
+                            const item2 = merchant.bank[pack2][slot2]
+                            const pack3 = `items${Math.floor((d[i + 2]) / 42)}` as Exclude<AL.BankPackName, "gold">
+                            const slot3 = d[i + 2] % 42
+                            const item3 = merchant.bank[pack3][slot3]
+
+                            const level0Grade = gInfo.grades.lastIndexOf(0) + 1
+                            if (item1.level >= 4 - level0Grade) continue // We don't want to comopound high level items automaticaclly
+                            if (item1.level !== item2.level) continue
+                            if (item1.level !== item3.level) continue
+
+                            // Withdraw the three items
+                            try {
+                                await merchant.withdrawItem(pack1, slot1)
+                                freeSpaces--
+                                await merchant.withdrawItem(pack2, slot2)
+                                freeSpaces--
+                                await merchant.withdrawItem(pack3, slot3)
+                                freeSpaces--
+                            } catch (e) {
+                                console.error(e)
+                            }
+
+                            // Remove the three items from the array
+                            d.splice(i, 3)
+                            i = i - 1
+                            break
+                        }
+                    }
+                }
+
+                // Withdraw things we want to hold
+                // TODO: improve to stack items that are stackable
+                for (let i = 0; i < bankItems.length && freeSpaces > 2; i++) {
+                    const item = bankItems[i]
+                    if (!item) continue // No item
+
+                    if (!MERCHANT_ITEMS_TO_HOLD.includes(item.name)) continue // We don't want to hold this item
+                    if (merchant.hasItem(item.name)) continue // We are already holding one of these items
+
+                    const pack = `items${Math.floor(i / 42)}` as Exclude<AL.BankPackName, "gold">
+                    const slot = i % 42
+                    merchant.withdrawItem(pack, slot)
+                    freeSpaces--
+                }
+
+                setTimeout(async () => { moveLoop() }, 250)
+                return
+            }
+
+            // MLuck people if there is a server info target
+            for (const mN in merchant.S) {
+                const type = mN as AL.MonsterName
+                if (!merchant.S[type].live) continue
+                if (!merchant.S[type].target) continue
+
+                if (AL.Tools.distance(merchant, merchant.S[type]) > 100) {
+                    await merchant.closeMerchantStand()
+                    await merchant.smartMove(merchant.S[type], { getWithin: 100 })
+                }
+
+                setTimeout(async () => { moveLoop() }, 250)
+                return
+            }
+
+            // mluck our friends
+            if (merchant.canUse("mluck")) {
+                for (const friend of [ranger, priest, rogue]) {
+                    if(!friend.s.mluck || !friend.s.mluck.strong || friend.s.mluck.ms < 120000) {
+                        // Move to them, and we'll automatically mluck them
+                        if (AL.Tools.distance(merchant, friend) > merchant.G.skills.mluck.range) {
+                            await merchant.closeMerchantStand()
+                            console.log(`[merchant] We are moving to ${friend.name} to mluck them!`)
+                            await merchant.smartMove(friend, { getWithin: merchant.G.skills.mluck.range / 2 })
+                        }
+                    }
+
+                    setTimeout(async () => { moveLoop() }, 250)
+                    return
+                }
+            }
+
+            // Go fishing if we can
+            if (merchant.getCooldown("fishing") == 0 /* Fishing is available */
+                && (merchant.hasItem("rod") || merchant.isEquipped("rod")) /* We have a rod */) {
+                let wasEquippedMainhand = merchant.slots.mainhand
+                let wasEquippedOffhand = merchant.slots.offhand
+                if (wasEquippedOffhand) await merchant.unequip("offhand") // rod is a 2-handed weapon, so we need to unequip our offhand if we have something equipped
+                else if (merchant.hasItem("wbook1")) wasEquippedOffhand = { name: "wbook1" } // We want to equip a wbook1 by default if we have one after we go fishing
+                if (wasEquippedMainhand) {
+                    if (wasEquippedMainhand.name !== "rod") {
+                        // We didn't have a rod equipped before, let's equip one now
+                        await merchant.unequip("mainhand")
+                        await merchant.equip(merchant.locateItem("rod"))
+                    }
+                } else {
+                    // We didn't have anything equipped before
+                    if (merchant.hasItem("dartgun")) wasEquippedMainhand = { name: "dartgun" } // We want to equip a dartgun by default if we have one after we go fishing
+                    await merchant.equip(merchant.locateItem("rod")) // Equip the rod
+                }
+                await merchant.smartMove({ map: "main", x: -1368, y: 0 }) // Move to fishing sppot
+                await merchant.fish()
+                if (wasEquippedMainhand) await merchant.equip(merchant.locateItem(wasEquippedMainhand.name))
+                if (wasEquippedOffhand) await merchant.equip(merchant.locateItem(wasEquippedOffhand.name))
+            }
+
+            // Hang out in town
             await merchant.smartMove("main")
             await merchant.openMerchantStand()
         } catch (e) {
             console.error(e)
         }
 
-        setTimeout(async () => { moveLoop() }, MOVE_TIME_MS)
+        setTimeout(async () => { moveLoop() }, 250)
     }
     moveLoop()
 }
