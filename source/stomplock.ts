c35fd26480ed2c398e0be54d3714bec9cdef712b
Kent Rasmussen <hyprkookeez@gmail.com>
2021-05-19 20:30:03 +0900

    Fix mongoose type issues

---
+++ source/stomplock.ts
@@ -0,0 +1,13 @@
+import AL from "alclient-mongo"
+
+/** Config */
+const warriorName = "earthWar"
+const warrior2Name = "earthWar2"
+const region: AL.ServerRegion = "US"
+const identifier: AL.ServerIdentifier = "II"
+
+const partyLeader = "earthWar"
+const partyMembers = [warriorName, warrior2Name,
+    // Lolwutpear's characters
+    ""
+]
