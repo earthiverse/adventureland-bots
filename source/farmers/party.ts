f5a7394a1a0294b0eb7568d069cfb599988244b6
Kent Rasmussen <hyprkookeez@gmail.com>
2021-07-07 18:58:30 +0900

    Remove merchants from party lists

--- source/farmers/party.ts
+++ source/farmers/party.ts
@@ -1,14 +1,12 @@
-import { ANNOUNCEMENT_CHARACTERS, KOUIN_CHARACTERS, LOLWUTPEAR_CHARACTERS, MY_CHARACTERS } from "../base/general.js"
-
 export const partyLeader = "earthiverse"
 /** NOTE: This is in order from high -> low priority. If the party becomes full, lower priority members could be kicked to make room for higher priority members. */
 export const partyMembers = [
     // Earthiverse's characters
-    ...MY_CHARACTERS,
+    "earthiverse", "earthMag", "earthMag2", "earthMag3", "earthPal", "earthPri", "earthPri2", "earthRan2", "earthRan3", "earthRog", "earthRog2", "earthWar", "earthWar2", "earthWar3",
     // Kouin's characters
-    ...KOUIN_CHARACTERS,
+    "bataxedude", "cclair", "fathergreen", "kakaka", "kekeke", "kukuku", "piredude",
     // Lolwutpear's characters
-    ...LOLWUTPEAR_CHARACTERS,
+    "fgsfds", "fsjal", "funny", "lolwutpear", "shoopdawhoop", "ytmnd",
     // Announcement's characters
-    ...ANNOUNCEMENT_CHARACTERS
+    "announcement", "battleworthy", "charmingness", "enlightening", "facilitating", "gratuitously", "hypothesized"
 ]
