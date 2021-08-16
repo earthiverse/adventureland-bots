e1fba2f062d32acf8310a1f221c9e73a5d7277cc
Kent Rasmussen <hyprkookeez@gmail.com>
2021-08-17 07:50:26 +0900

    Add try/catch to give while loop a chance

--- source/videos/optimizing_attack/3_while_true.js
+++ source/videos/optimizing_attack/3_while_true.js
@@ -10,23 +10,27 @@ character.on("target_hit", (data) => { if (data.kill) numKilled += 1 })
 /** NOTE: This is a bad idea, and eslint tells us that, but we're going to ignore the warnings */
 // eslint-disable-next-line no-constant-condition
 while (true) {
-    numCalls += 1
-    if (started == undefined) started = Date.now()
-    if (Date.now() > started + TenMinutesInMs) {
-        show_json(`We killed ${numKilled} monsters. We called this function ${numCalls} times.`)
-        started = Date.now()
-        numKilled = 0
-        numCalls = 0
-    }
+    try {
+        numCalls += 1
+        if (started == undefined) started = Date.now()
+        if (Date.now() > started + TenMinutesInMs) {
+            show_json(`We killed ${numKilled} monsters. We called this function ${numCalls} times.`)
+            started = Date.now()
+            numKilled = 0
+            numCalls = 0
+        }
 
-    const nearest = get_nearest_monster()
-    if (!nearest) {
-        set_message("No Monsters")
-        return
-    }
+        const nearest = get_nearest_monster()
+        if (!nearest) {
+            set_message("No Monsters")
+            return
+        }
 
-    if (can_attack(nearest)) {
-        set_message("Attacking")
-        attack(nearest)
+        if (can_attack(nearest)) {
+            set_message("Attacking")
+            attack(nearest)
+        }
+    } catch (e) {
+        console.error(e)
     }
 }
