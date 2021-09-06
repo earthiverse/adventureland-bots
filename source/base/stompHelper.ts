13862c390f2213c6052886b1ee1822115dba088f
Kent Rasmussen <hyprkookeez@gmail.com>
2021-09-06 15:33:49 +0900

    Move to bottom.

---
+++ source/base/stompHelper.ts
@@ -0,0 +1,60 @@
+import AL, { CMData } from "alclient"
+import { LOOP_MS } from "./general"
+
+/**
+ * NOTE: Not implemented yet.
+ * The idea is to have one bot be a 'stompController' and have other bots be 'stompHelpers'.
+ */
+
+export function startStompController(bot: AL.Warrior): void {
+    const numHelpers = 0
+
+    async function stompControllerLoop() {
+        // TODO: Implement
+    }
+}
+
+export function startStompHelper(bot: AL.Warrior, controller: string): void {
+    // TODO: Get updated information from controller
+    const stompSettings = {
+        range: 100
+    }
+
+    // Listen for CMs
+    bot.socket.on("cm", (data: CMData) => {
+        if (data.name !== controller) return // Message is not from our controller
+
+        try {
+            // Parse the message
+            const newStompSettings = JSON.parse(data.message)
+
+            // Verify the message
+            // TODO
+
+            // Set new settings
+            for (const newSetting in newStompSettings) {
+                stompSettings[newSetting] = newStompSettings[newSetting]
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    })
+
+    async function stompLoop() {
+        try {
+            if (!bot.socket || bot.socket.disconnected) return
+
+            if (
+                bot.players.has(controller)
+                && AL.Tools.distance(bot.players.get(controller), bot) <= stompSettings.range
+            ) {
+                // TODO: Implement this
+            }
+        } catch (e) {
+            console.error(e)
+        }
+
+        bot.timeouts.set("stomphelperloop", setTimeout(async () => { stompLoop() }, Math.max(LOOP_MS, bot.getCooldown("stomp"))))
+    }
+    stompLoop()
+}
