b1d2919210e4401ed6228e6a331fb1e0685091fb
Kent Rasmussen <hyprkookeez@gmail.com>
2021-09-27 06:27:52 +0900

    Tweak loops

--- source/spiders/lolwutpear.ts
+++ source/spiders/lolwutpear.ts
@@ -97,146 +97,132 @@ async function run() {
     console.log("Connecting...")
 
     const startWarrior1Loop = async () => {
-        // Start the characters
         const loopBot = async () => {
-            try {
-                if (information.bot1.bot) await information.bot1.bot.disconnect()
-                information.bot1.bot = await AL.Game.startWarrior(information.bot1.name, TARGET_REGION, TARGET_IDENTIFIER)
-                information.friends[1] = information.bot1.bot
-                startWarrior(information.bot1.bot as AL.Warrior)
-                startTrackerLoop(information.bot1.bot)
-                information.bot1.bot.socket.on("disconnect", async () => { loopBot() })
-            } catch (e) {
-                console.error(e)
-                if (information.bot1.bot) await information.bot1.bot.disconnect()
-                const wait = /wait_(\d+)_second/.exec(e)
-                if (wait && wait[1]) {
-                    setTimeout(async () => { loopBot() }, 1000 + Number.parseInt(wait[1]) * 1000)
-                } else if (/limits/.test(e)) {
-                    setTimeout(async () => { loopBot() }, AL.Constants.RECONNECT_TIMEOUT_MS)
-                } else if (/ingame/.test(e)) {
-                    setTimeout(async () => { loopBot() }, 500)
-                } else {
-                    setTimeout(async () => { loopBot() }, 10000)
+            const connectLoop = async () => {
+                try {
+                    if (information.bot1.bot) await information.bot1.bot.disconnect()
+                    information.bot1.bot = await AL.Game.startWarrior(information.bot1.name, TARGET_REGION, TARGET_IDENTIFIER)
+                    information.friends[1] = information.bot1.bot
+                    startWarrior(information.bot1.bot as AL.Warrior)
+                    information.bot1.bot.socket.on("disconnect", async () => { loopBot() })
+                } catch (e) {
+                    console.error(e)
+                    if (information.bot1.bot) await information.bot1.bot.disconnect()
                 }
+                const msToNextMinute = 60_000 - (Date.now() % 60_000)
+                setTimeout(async () => { connectLoop() }, msToNextMinute + 5000)
             }
+
+            const disconnectLoop = async () => {
+                try {
+                    if (information.bot1.bot) await information.bot1.bot.disconnect()
+                    information.bot1.bot = undefined
+                } catch (e) {
+                    console.error(e)
+                }
+                const msToNextMinute = 60_000 - (Date.now() % 60_000)
+                setTimeout(async () => { disconnectLoop() }, msToNextMinute - 5000 < 0 ? msToNextMinute + 55_000 : msToNextMinute - 5000)
+            }
+
+            const msToNextMinute = 60_000 - (Date.now() % 60_000)
+            setTimeout(async () => { connectLoop() }, msToNextMinute + 5000)
+            setTimeout(async () => { disconnectLoop() }, msToNextMinute - 5000 < 0 ? msToNextMinute + 55_000 : msToNextMinute - 5000)
         }
         loopBot()
     }
     startWarrior1Loop().catch(() => { /* ignore errors */ })
 
     const startWarrior2Loop = async () => {
-        // Start the characters
         const loopBot = async () => {
-            try {
-                if (information.bot2.bot) await information.bot2.bot.disconnect()
-                information.bot2.bot = await AL.Game.startWarrior(information.bot2.name, TARGET_REGION, TARGET_IDENTIFIER)
-                information.friends[2] = information.bot2.bot
-                startWarrior(information.bot2.bot as AL.Warrior)
-                information.bot2.bot.socket.on("disconnect", async () => { loopBot() })
-            } catch (e) {
-                console.error(e)
-                if (information.bot2.bot) await information.bot2.bot.disconnect()
-                const wait = /wait_(\d+)_second/.exec(e)
-                if (wait && wait[1]) {
-                    setTimeout(async () => { loopBot() }, 1000 + Number.parseInt(wait[1]) * 1000)
-                } else if (/limits/.test(e)) {
-                    setTimeout(async () => { loopBot() }, AL.Constants.RECONNECT_TIMEOUT_MS)
-                } else if (/ingame/.test(e)) {
-                    setTimeout(async () => { loopBot() }, 500)
-                } else {
-                    setTimeout(async () => { loopBot() }, 10000)
+            const connectLoop = async () => {
+                try {
+                    if (information.bot2.bot) await information.bot2.bot.disconnect()
+                    information.bot2.bot = await AL.Game.startWarrior(information.bot2.name, TARGET_REGION, TARGET_IDENTIFIER)
+                    information.friends[2] = information.bot2.bot
+                    startWarrior(information.bot2.bot as AL.Warrior)
+                    information.bot2.bot.socket.on("disconnect", async () => { loopBot() })
+                } catch (e) {
+                    console.error(e)
+                    if (information.bot2.bot) await information.bot2.bot.disconnect()
                 }
+                const msToNextMinute = 60_000 - (Date.now() % 60_000)
+                setTimeout(async () => { connectLoop() }, msToNextMinute + 5000)
             }
+
+            const disconnectLoop = async () => {
+                try {
+                    if (information.bot2.bot) await information.bot2.bot.disconnect()
+                    information.bot2.bot = undefined
+                } catch (e) {
+                    console.error(e)
+                }
+                const msToNextMinute = 60_000 - (Date.now() % 60_000)
+                setTimeout(async () => { disconnectLoop() }, msToNextMinute - 5000 < 0 ? msToNextMinute + 55_000 : msToNextMinute - 5000)
+            }
+
+            const msToNextMinute = 60_000 - (Date.now() % 60_000)
+            setTimeout(async () => { connectLoop() }, msToNextMinute + 5000)
+            setTimeout(async () => { disconnectLoop() }, msToNextMinute - 5000 < 0 ? msToNextMinute + 55_000 : msToNextMinute - 5000)
         }
         loopBot()
     }
     startWarrior2Loop().catch(() => { /* ignore errors */ })
 
     const startWarrior3Loop = async () => {
-        // Start the characters
         const loopBot = async () => {
-            try {
-                if (information.bot3.bot) await information.bot3.bot.disconnect()
-                information.bot3.bot = await AL.Game.startWarrior(information.bot3.name, TARGET_REGION, TARGET_IDENTIFIER)
-                information.friends[3] = information.bot3.bot
-                startWarrior(information.bot3.bot as AL.Warrior)
-                information.bot3.bot.socket.on("disconnect", async () => { loopBot() })
-            } catch (e) {
-                console.error(e)
-                if (information.bot3.bot) await information.bot3.bot.disconnect()
-                const wait = /wait_(\d+)_second/.exec(e)
-                if (wait && wait[1]) {
-                    setTimeout(async () => { loopBot() }, 1000 + Number.parseInt(wait[1]) * 1000)
-                } else if (/limits/.test(e)) {
-                    setTimeout(async () => { loopBot() }, AL.Constants.RECONNECT_TIMEOUT_MS)
-                } else if (/ingame/.test(e)) {
-                    setTimeout(async () => { loopBot() }, 500)
-                } else {
-                    setTimeout(async () => { loopBot() }, 10000)
+            const connectLoop = async () => {
+                try {
+                    if (information.bot3.bot) await information.bot3.bot.disconnect()
+                    information.bot3.bot = await AL.Game.startWarrior(information.bot3.name, TARGET_REGION, TARGET_IDENTIFIER)
+                    information.friends[3] = information.bot3.bot
+                    startWarrior(information.bot3.bot as AL.Warrior)
+                    information.bot3.bot.socket.on("disconnect", async () => { loopBot() })
+                } catch (e) {
+                    console.error(e)
+                    if (information.bot3.bot) await information.bot3.bot.disconnect()
                 }
+                const msToNextMinute = 60_000 - (Date.now() % 60_000)
+                setTimeout(async () => { connectLoop() }, msToNextMinute + 5000)
             }
+
+            const disconnectLoop = async () => {
+                try {
+                    if (information.bot3.bot) await information.bot3.bot.disconnect()
+                    information.bot3.bot = undefined
+                } catch (e) {
+                    console.error(e)
+                }
+                const msToNextMinute = 60_000 - (Date.now() % 60_000)
+                setTimeout(async () => { disconnectLoop() }, msToNextMinute - 5000 < 0 ? msToNextMinute + 55_000 : msToNextMinute - 5000)
+            }
+
+            const msToNextMinute = 60_000 - (Date.now() % 60_000)
+            setTimeout(async () => { connectLoop() }, msToNextMinute + 5000)
+            setTimeout(async () => { disconnectLoop() }, msToNextMinute - 5000 < 0 ? msToNextMinute + 55_000 : msToNextMinute - 5000)
         }
         loopBot()
     }
     startWarrior3Loop().catch(() => { /* ignore errors */ })
 
-    let lastServerChangeTime = Date.now()
-    const serverLoop = async () => {
+    const targetServerLoop = async () => {
         try {
-            // We haven't logged in yet
-            if (!information.bot1.bot) {
-                setTimeout(async () => { serverLoop() }, 1000)
-                return
-            }
-
-            // Don't change servers too fast
-            if (lastServerChangeTime > Date.now() - 60_000) {
-                setTimeout(async () => { serverLoop() }, Math.max(1000, lastServerChangeTime + AL.Constants.RECONNECT_TIMEOUT_MS - Date.now()))
-                return
-            }
-
-            const currentRegion = information.bot1.bot.server.region
-            const currentIdentifier = information.bot1.bot.server.name
-
             const targetServer = await getTargetServerFromPlayer(TARGET_REGION, TARGET_IDENTIFIER, TARGET_PLAYER)
-            if (currentRegion == targetServer[0] && currentIdentifier == targetServer[1]) {
-                // We're already on the correct server
-                setTimeout(async () => { serverLoop() }, 1000)
-                return
-            }
 
             if (targetServer[0] == "US" && targetServer[1] == "III") {
                 // Avoid this server
-                setTimeout(async () => { serverLoop() }, 1000)
+                setTimeout(async () => { targetServerLoop() }, 1000)
                 return
             }
 
             // Change servers to attack this entity
             TARGET_REGION = targetServer[0]
             TARGET_IDENTIFIER = targetServer[1]
-            console.log(`Changing from ${currentRegion} ${currentIdentifier} to ${TARGET_REGION} ${TARGET_IDENTIFIER}`)
-
-            // Loot all of our remaining chests
-            await sleep(1000)
-            for (const [, chest] of information.bot1.bot?.chests) await information.bot1.bot.openChest(chest.id)
-            await sleep(1000)
-
-            // Disconnect everyone
-            await Promise.race([Promise.allSettled([
-                information.bot1.bot?.disconnect(),
-                information.bot2.bot?.disconnect(),
-                information.bot3.bot?.disconnect(),
-                information.merchant.bot?.disconnect()
-            ]), new Promise((_resolve, reject) => { setTimeout(reject, 30_000, "Bots didn't disconnect within 30s") })])
-            await sleep(5000)
-            lastServerChangeTime = Date.now()
         } catch (e) {
             console.error(e)
         }
 
-        setTimeout(async () => { serverLoop() }, 1000)
+        setTimeout(async () => { targetServerLoop() }, 1000)
     }
-    serverLoop()
+    targetServerLoop()
 }
 run()
