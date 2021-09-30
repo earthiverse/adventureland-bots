43ed3ddac0b23bd345aeac0abcb8318c9653f4e2
Kent Rasmussen <hyprkookeez@gmail.com>
2021-09-30 12:58:36 +0900

    Keep more items

--- source/frogs/earthiverse.ts
+++ source/frogs/earthiverse.ts
@@ -13,7 +13,7 @@ async function startMage(bot: AL.Mage) {
     startBuyLoop(bot, new Set())
     startHealLoop(bot)
     startLootLoop(bot)
-    startSellLoop(bot, { "hpamulet": 2, "hpbelt": 2, "ringsj": 2, "shield": 2, "wcap": 2, "wshoes": 2 })
+    startSellLoop(bot, { "hpamulet": 2, "hpbelt": 2, "ringsj": 2 })
     startPartyLoop(bot, "earthPri2", MY_CHARACTERS)
 
     async function attackLoop() {
--- source/hens/earthiverse.ts
+++ source/hens/earthiverse.ts
@@ -14,7 +14,7 @@ async function startWarrior(bot: AL.Warrior) {
     startBuyLoop(bot, new Set())
     startHealLoop(bot)
     startLootLoop(bot)
-    startSellLoop(bot, { "hpamulet": 2, "hpbelt": 2, "ringsj": 2, "shield": 2, "wcap": 2, "wshoes": 2 })
+    startSellLoop(bot, { "hpamulet": 2, "hpbelt": 2, "ringsj": 2 })
     startPartyLoop(bot, "earthPri2", MY_CHARACTERS)
 
     async function attackLoop() {
