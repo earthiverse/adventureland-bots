88d87319a8b14a589429500dcf449a0cb0d7cf7d
Kent Rasmussen <hyprkookeez@gmail.com>
2021-10-18 06:33:28 +0900

    Improve achievement pruning

--- source/tools/prune_achievements.ts
+++ source/tools/prune_achievements.ts
@@ -1,27 +1,22 @@
 import AL from "alclient"
-import Queue from "queue-promise"
 
 const ONE_HOUR_IN_MS = 3.6e+6
 const ONE_DAY_IN_MS = 8.64e+7
 const ONE_WEEK_IN_MS = 6.048e+8
 const ONE_MONTH_IN_MS = 2.628e+9
-const ONE_YEAR_IN_MS = 3.154e+10
+const ONE_YEAR_IN_MS = 3.156e+10
 
 AL.Game.loginJSONFile("../../credentials.json").then(async () => {
     const oneWeekAgo = Date.now() - ONE_WEEK_IN_MS
     const oneMonthAgo = Date.now() - ONE_MONTH_IN_MS
     const oneYearAgo = Date.now() - ONE_YEAR_IN_MS
 
-    const queue = new Queue({
-        concurrent: 2,
-        interval: AL.Constants.TIMEOUT
-    })
-
     let lastName = undefined
     let lastDate = undefined
+    const idsToDelete: string[] = []
     console.log("Pruning achievements...")
     // eslint-disable-next-line sort-keys
-    for (const achievement of await AL.AchievementModel.find({}, { _id: 0, name: 1, date: 1 }).sort({ name: 1, date: -1 }).lean().exec()) {
+    for (const achievement of await AL.AchievementModel.find({}, { _id: 1, name: 1, date: 1 }).sort({ name: 1, date: -1 }).lean().exec()) {
         if (achievement.name !== lastName) {
             // This is the latest data we have for the user, we want to keep it.
             lastName = achievement.name
@@ -29,49 +24,47 @@ AL.Game.loginJSONFile("../../credentials.json").then(async () => {
             continue
         }
 
-        if (achievement.date > oneWeekAgo) {
-            // Keep hourly
-            if (lastDate - achievement.date < ONE_HOUR_IN_MS) {
-                // Delete this hourly data
-                queue.enqueue(async () => {
-                    console.log(`deleting ${achievement.name} @ ${achievement.date} (within 1 week, same hour)`)
-                    await AL.AchievementModel.deleteOne({ date: achievement.date, name: achievement.name }).lean().exec()
-                })
-            }
-        } else if (achievement.date > oneMonthAgo) {
-            // Keep daily
-            if (lastDate - achievement.date < ONE_DAY_IN_MS) {
-                // Delete this hourly data
-                queue.enqueue(async () => {
-                    console.log(`deleting ${achievement.name} @ ${achievement.date} (within 1 month, same day)`)
-                    await AL.AchievementModel.deleteOne({ date: achievement.date, name: achievement.name }).lean().exec()
-                })
-            }
-        } else if (achievement.date > oneYearAgo) {
-            // Keep weekly
+        if (achievement.date <= oneYearAgo) {
+            // Over 1 year old -- Keep weekly
             if (lastDate - achievement.date < ONE_WEEK_IN_MS) {
                 // Delete this hourly data
-                queue.enqueue(async () => {
-                    console.log(`deleting ${achievement.name} @ ${achievement.date} (within 1 year, same week)`)
-                    await AL.AchievementModel.deleteOne({ date: achievement.date, name: achievement.name }).lean().exec()
-                })
+                console.log(`deleting ${achievement.name} @ ${achievement.date} (over 1 year, same week)`)
+                idsToDelete.push(achievement._id)
+                continue
+            }
+        } else if (achievement.date <= oneMonthAgo) {
+            // Over one month old -- Keep daily
+            if (lastDate - achievement.date < ONE_DAY_IN_MS) {
+                // Delete this hourly data
+                console.log(`deleting ${achievement.name} @ ${achievement.date} (over 1 month, same day)`)
+                idsToDelete.push(achievement._id)
+                continue
+            }
+        } else if (achievement.date <= oneWeekAgo) {
+            // Over one week old -- Keep hourly
+            if (lastDate - achievement.date < ONE_HOUR_IN_MS) {
+                // Delete this hourly data
+                console.log(`deleting ${achievement.name} @ ${achievement.date} (over 1 week, same hour)`)
+                idsToDelete.push(achievement._id)
+                continue
             }
         } else {
-            // Keep monthly
-            if (lastDate - achievement.date < ONE_MONTH_IN_MS) {
-                // Delete this hourly data
-                queue.enqueue(async () => {
-                    console.log(`deleting ${achievement.name} @ ${achievement.date} (over 1 year, same month)`)
-                    await AL.AchievementModel.deleteOne({ date: achievement.date, name: achievement.name }).lean().exec()
-                })
+            // Less than 1 week old -- Keep 15 minutes
+            if (lastDate - achievement.date < 900000) {
+                // Delete this 15 minute data
+                console.log(`deleting ${achievement.name} @ ${achievement.date} (within 1 week, same 15 minute interval)`)
+                idsToDelete.push(achievement._id)
+                continue
             }
         }
+
+        // We want to keep this achievement
+        lastDate = achievement.date
     }
 
-    queue.on("end", () => {
-        AL.Database.disconnect()
-        queue.stop()
-    })
-    if (queue.shouldRun) queue.start()
-    else AL.Database.disconnect()
+    await AL.AchievementModel.deleteMany({
+        _id: { $in: idsToDelete }
+    }).lean().exec()
+
+    AL.Database.disconnect()
 })
