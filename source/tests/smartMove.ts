c2f40514c289c86837fd499d40c4d2608abb61df
Kent Rasmussen <hyprkookeez@gmail.com>
2021-10-23 19:02:01 +0900

    Update ALClient

--- source/tests/smartMove.ts
+++ source/tests/smartMove.ts
@@ -12,19 +12,26 @@ async function run() {
     const standable = await AL.Pathfinder.canStand({ map: "winterland", x: -280, y: -123 })
     console.log(standable)
 
-    const bot = await AL.Game.startCharacter("earthPal", "ASIA", "I")
-    try {
-        let data2 = await bot.smartMove({ map: "winterland", x: -280, y: -123 })
-        console.log(data2)
-        data2 = await bot.move(-280, -123)
-        console.log(data2)
-        // data2 = await bot.smartMove("elixirluck", { getWithin: AL.Constants.NPC_INTERACTION_DISTANCE - 1 })
-        // console.log(data2)
-        data2 = await bot.smartMove("monsterhunter", { getWithin: AL.Constants.NPC_INTERACTION_DISTANCE - 1 })
-        console.log(data2)
-    } catch (e) {
-        console.error(e)
-    }
-    bot.disconnect()
+    // const bot = await AL.Game.startCharacter("earthMer3", "ASIA", "I")
+    // try {
+    //     let data2 = await bot.smartMove({ map: "winterland", x: -280, y: -123 })
+    //     console.log(data2)
+    //     data2 = await bot.move(-280, -123)
+    //     console.log(data2)
+    //     // data2 = await bot.smartMove("elixirluck", { getWithin: AL.Constants.NPC_INTERACTION_DISTANCE - 1 })
+    //     // console.log(data2)
+    //     data2 = await bot.smartMove("monsterhunter", { getWithin: AL.Constants.NPC_INTERACTION_DISTANCE - 1 })
+    //     console.log(data2)
+    // } catch (e) {
+    //     console.error(e)
+    // }
+
+    const canWalk = AL.Pathfinder.canWalkPath({ map: "main", x: 17, y: -152 }, { map: "main", x: 0, y: 0 })
+    console.log(`canWalk: ${canWalk}`)
+
+    const data2 = await AL.Pathfinder.getPath({ map: "main", x: 17, y: -152 }, { map: "main", x: 383, y: 1480 }, { avoidTownWarps: true, costs: { town: 11450 } })
+    console.log(data2)
+
+    // bot.disconnect()
 }
 run()
