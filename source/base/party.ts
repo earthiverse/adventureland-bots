fce9a2b5403e20540b2fa782d462f9a4a95553ce
Kent Rasmussen <hyprkookeez@gmail.com>
2021-10-25 20:10:26 +0900

    Fix party leader

--- source/base/party.ts
+++ source/base/party.ts
@@ -1,4 +1,4 @@
-export const partyLeader = "earthiverse"
+export const partyLeader = "earthMag"
 /** NOTE: This is in order from high -> low priority. If the party becomes full, lower priority members could be kicked to make room for higher priority members. */
 export const partyMembers = [
     // Earthiverse's characters
