f768b856e8247add97f216c2575eb3ba86579665
Kent Rasmussen <hyprkookeez@gmail.com>
2021-10-27 08:11:41 +0900

    Swap servers if we spend too much time looking

--- source/slenderman/earthiverse.ts
+++ source/slenderman/earthiverse.ts
@@ -199,8 +199,8 @@ async function run() {
                 return
             }
 
-            // Don't change servers if slender is live
-            if (mage1.S?.slenderman && mage1.S.slenderman.live) {
+            // Don't change servers if slender is live, and we haven't spent a lot of time on the server looking for him
+            if (mage1.S?.slenderman && mage1.S.slenderman.live && lastServerChangeTime > (Date.now() - 900_000)) {
                 setTimeout(async () => { serverLoop() }, 1000)
                 return
             }
