dd81ca4959de1fe65e5721a23070192ba9fa9615
Kent Rasmussen <hyprkookeez@gmail.com>
2021-10-26 20:47:42 +0900

    Random everything!

--- source/slenderman/earthiverse.ts
+++ source/slenderman/earthiverse.ts
@@ -1,28 +1,29 @@
-import AL, { GMap, IPosition, Mage, MapName, Merchant, ServerIdentifier, ServerRegion } from "alclient"
+import AL, { IPosition, Mage, MonsterName, ServerIdentifier, ServerRegion } from "alclient"
 import { goToPotionSellerIfLow, startBuyLoop, startHealLoop, startLootLoop, startSellLoop, goToBankIfFull, ITEMS_TO_SELL, startPartyLoop, startScareLoop, startAvoidStacking } from "../base/general.js"
-import { offsetPosition } from "../base/locations.js"
 import { attackTheseTypesMage } from "../base/mage.js"
 import { partyLeader, partyMembers } from "../base/party.js"
 
 /** Config */
 const region: ServerRegion = "US"
 const identifier: ServerIdentifier = "II"
+const monsters: MonsterName[] = ["bat", "minimush", "snake", "stoneworm", "osnake", "mvampire", "fvampire", "jr", "greenjr"]
 
 const mage1Name = "earthMag"
-const mage1Position: IPosition = { map: "cave", x: -123, y: -456 }
 const mage2Name = "earthMag2"
-const mage2Position: IPosition = { map: "cave", x: 234, y: -1098 }
 const mage3Name = "earthMag3"
-const mage3Position: IPosition = { map: "cave", x: 1234, y: -765 }
-const merchantName = "earthMer"
-const merchantMap: MapName = "cave"
 
 let mage1: Mage
 let mage2: Mage
 let mage3: Mage
-let merchant: Merchant
 
-async function startMage(bot: Mage, position: IPosition) {
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+async function startMage(bot: Mage) {
+    const locations: IPosition[] = []
+    for (const monster of monsters) locations.push(...bot.locateMonster(monster))
+
     startAvoidStacking(bot)
     startBuyLoop(bot, new Set())
     startHealLoop(bot)
@@ -45,7 +46,7 @@ async function startMage(bot: Mage, position: IPosition) {
                 // NOTE: We are bursting in the move loop, because we can do it really fast there
                 if (bot.canUse("attack")) await bot.basicAttack(slenderman.id).catch(() => { /** Suppress warnings */ })
             } else {
-                await attackTheseTypesMage(bot, ["bat", "goldenbat"], [], { disableCburst: true, disableEnergize: true })
+                await attackTheseTypesMage(bot, monsters, [], { disableCburst: true, disableEnergize: true })
             }
         } catch (e) {
             console.error(e)
@@ -75,59 +76,7 @@ async function startMage(bot: Mage, position: IPosition) {
                 if (bot.canUse("blink")) bot.blink(slenderman.x, slenderman.y).catch(() => { /** Suppress warnings */ })
                 if (bot.canUse("burst")) bot.burst(slenderman.id).catch(() => { /** Suppress warnings */ })
             } else if (!bot.smartMoving) {
-                const randomPos = offsetPosition(position, (Math.random() - 0.5) * 20, (Math.random() - 0.5) * 20)
-                bot.smartMove(randomPos).catch(() => { /** Suppress warnings */ })
-            }
-        } catch (e) {
-            console.error(e)
-        }
-
-        bot.timeouts.set("moveLoop", setTimeout(async () => { moveLoop() }, 250))
-    }
-    moveLoop()
-}
-
-async function startMerchant(bot: Merchant) {
-    startBuyLoop(bot, new Set())
-    startHealLoop(bot)
-    startLootLoop(bot)
-    startScareLoop(bot)
-    startSellLoop(bot, { ...ITEMS_TO_SELL, "wbook0": 2 })
-
-    // We're not AFK, I swear...
-    setInterval(() => {
-        bot.socket.emit("property", { "afk": false })
-    }, 10_000)
-
-    const locations: IPosition[] = []
-    for (const monsterSpawn of (bot.G.maps[merchantMap] as GMap).monsters) {
-        if (monsterSpawn.boundary) {
-            locations.push({ "map": merchantMap as MapName, "x": (monsterSpawn.boundary[0] + monsterSpawn.boundary[2]) / 2, "y": (monsterSpawn.boundary[1] + monsterSpawn.boundary[3]) / 2 })
-        } else if (monsterSpawn.boundaries) {
-            for (const boundary of monsterSpawn.boundaries) {
-                if (boundary[0] !== merchantMap) continue
-                locations.push({ "map": boundary[0], "x": (boundary[1] + boundary[3]) / 2, "y": (boundary[2] + boundary[4]) / 2 })
-            }
-        }
-    }
-
-    async function moveLoop() {
-        try {
-            if (!bot.socket || bot.socket.disconnected) return
-
-            // If we are dead, respawn
-            if (bot.rip) {
-                await bot.respawn()
-                bot.timeouts.set("moveLoop", setTimeout(async () => { moveLoop() }, 250))
-                return
-            }
-
-            await goToPotionSellerIfLow(bot)
-            await goToBankIfFull(bot)
-
-            // Go to each spawn on the map, in a loop
-            for (const location of locations) {
-                await bot.smartMove(location, { getWithin: 550 })
+                bot.smartMove(locations[randomIntFromInterval(0, locations.length)]).catch(() => { /** Suppress warnings */ })
             }
         } catch (e) {
             console.error(e)
@@ -151,7 +100,7 @@ async function run() {
             try {
                 if (mage1) mage1.disconnect()
                 mage1 = await AL.Game.startMage(name, region, identifier)
-                startMage(mage1, mage1Position)
+                startMage(mage1)
                 mage1.socket.on("disconnect", async () => { loopBot() })
             } catch (e) {
                 console.error(e)
@@ -178,7 +127,7 @@ async function run() {
             try {
                 if (mage2) mage2.disconnect()
                 mage2 = await AL.Game.startMage(name, region, identifier)
-                startMage(mage2, mage2Position)
+                startMage(mage2)
                 mage2.socket.on("disconnect", async () => { loopBot() })
             } catch (e) {
                 console.error(e)
@@ -205,7 +154,7 @@ async function run() {
             try {
                 if (mage3) mage3.disconnect()
                 mage3 = await AL.Game.startMage(name, region, identifier)
-                startMage(mage3, mage3Position)
+                startMage(mage3)
                 mage3.socket.on("disconnect", async () => { loopBot() })
             } catch (e) {
                 console.error(e)
@@ -225,32 +174,5 @@ async function run() {
         loopBot()
     }
     startMage3Loop(mage3Name, region, identifier).catch(() => { /* ignore errors */ })
-
-    const startMerchantLoop = async (name: string, region: ServerRegion, identifier: ServerIdentifier) => {
-        // Start the characters
-        const loopBot = async () => {
-            try {
-                if (merchant) merchant.disconnect()
-                merchant = await AL.Game.startMerchant(name, region, identifier)
-                startMerchant(merchant)
-                merchant.socket.on("disconnect", async () => { loopBot() })
-            } catch (e) {
-                console.error(e)
-                if (merchant) merchant.disconnect()
-                const wait = /wait_(\d+)_second/.exec(e)
-                if (wait && wait[1]) {
-                    setTimeout(async () => { loopBot() }, 1000 + Number.parseInt(wait[1]) * 1000)
-                } else if (/limits/.test(e)) {
-                    setTimeout(async () => { loopBot() }, AL.Constants.RECONNECT_TIMEOUT_MS)
-                } else if (/ingame/.test(e)) {
-                    setTimeout(async () => { loopBot() }, 500)
-                } else {
-                    setTimeout(async () => { loopBot() }, 10000)
-                }
-            }
-        }
-        loopBot()
-    }
-    startMerchantLoop(merchantName, region, identifier).catch(() => { /* ignore errors */ })
 }
 run()
