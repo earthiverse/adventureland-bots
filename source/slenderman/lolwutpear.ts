0efa881616155a5c7f3150e1c585016141fb66a3
Kent Rasmussen <hyprkookeez@gmail.com>
2021-10-27 14:36:15 +0900

    Implement advanced strats?

--- source/slenderman/lolwutpear.ts
+++ source/slenderman/lolwutpear.ts
@@ -1,12 +1,14 @@
-import AL, { IPosition, Mage, MonsterName, ServerIdentifier, ServerRegion } from "alclient"
+import AL, { DeathData, GameResponseData, IPosition, Mage, MapName, MonsterName, ServerIdentifier, ServerInfoData, ServerRegion } from "alclient"
 import { goToPotionSellerIfLow, startBuyLoop, startHealLoop, startLootLoop, startSellLoop, goToBankIfFull, ITEMS_TO_SELL, startPartyLoop, startScareLoop, startAvoidStacking, sleep } from "../base/general.js"
 import { attackTheseTypesMage } from "../base/mage.js"
 import { partyLeader, partyMembers } from "../base/party.js"
-import { getTargetServerFromPlayer } from "../base/serverhop.js"
+import { getTargetServerFromCurrentServer } from "../base/serverhop.js"
+import { trilateration } from "node-trilateration"
 
 /** Config */
 let region: ServerRegion = "EU"
 let identifier: ServerIdentifier = "II"
+const toLookForMap: MapName = "spookytown"
 const toLookFor: MonsterName[] = ["bat", "minimush", "snake", "stoneworm", "phoenix", "ghost", "jr", "xscorpion", "mrgreen"]
 const extraToLook: IPosition[] = [{ map: "spookytown", x: 250, y: -1129 }, { map: "spookytown", x: -525, y: -715 }, { map: "halloween", x: 920, y: -120 }]
 const toAttack: MonsterName[] = ["bat", "bee", "goo", "goldenbat", "minimush", "snake", "scorpion", "stoneworm", "osnake", "jr", "greenjr"]
@@ -23,14 +25,45 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+let slenderID: string
+let slenderTrilateration: (IPosition & {distance: number})[] = []
+
 async function startMage(bot: Mage) {
     const locations: IPosition[] = extraToLook
     for (const monster of toLookFor) {
         for (const location of bot.locateMonster(monster)) {
-            if (location.map !== "cave" && location.map !== "spookytown" && location.map !== "halloween") continue
+            if (location.map !== toLookForMap) continue
             locations.push(location)
         }
     }
+
+    bot.socket.on("death", (data: DeathData) => {
+        if (data.id == slenderID) {
+            // Clear Slender Data
+            slenderID = undefined
+            slenderTrilateration = []
+        }
+    })
+    bot.socket.on("server_info", (data: ServerInfoData) => {
+        if (data.slenderman && !data.slenderman.live) {
+            // Clear Slender Data
+            slenderID = undefined
+            slenderTrilateration = []
+        }
+    })
+    bot.socket.on("game_response", (data: GameResponseData) => {
+        if (typeof data == "object") {
+            if (data.response == "too_far" && data.id == slenderID) {
+                for (const trilateration of slenderTrilateration) {
+                    if (trilateration.map !== bot.map) {
+                        // Found on a different map, clear the trilaterations
+                        slenderTrilateration = []
+                    }
+                }
+                slenderTrilateration.push({ distance: data.dist, map: bot.map, x: bot.x, y: bot.y })
+            }
+        }
+    })
 
     startAvoidStacking(bot)
     startBuyLoop(bot, new Set())
@@ -40,26 +73,53 @@ async function startMage(bot: Mage) {
     startSellLoop(bot, { ...ITEMS_TO_SELL, "wbook0": 2 })
     startPartyLoop(bot, partyLeader, partyMembers)
 
-    // We're not AFK, I swear...
-    setInterval(() => {
-        bot.socket.emit("property", { "afk": false })
-    }, 10_000)
+    async function trilaterationLoop() {
+        try {
+            if (slenderTrilateration.length >= 3) {
+                const map = slenderTrilateration[0].map
+                const position: {x: number, y: number} = trilateration.calculate([slenderTrilateration[0], slenderTrilateration[1], slenderTrilateration[2]])
+                console.log(`Slenderman trilaterated to ${map},${position.x},${position.y}.`)
+                for (const mage of [mage1, mage2, mage3]) {
+                    if (!mage) continue // Mage isn't up
+                    if (mage.map !== toLookForMap) continue // Mage isn't on the same map
+                    if (mage.canUse("blink")) {
+                        if (AL.Pathfinder.canStand({ map: map, x: position.x, y: position.y })) {
+                            mage.blink(position.x, position.y).catch(() => { /** Suppress warnings */ })
+                        } else {
+                            mage.move(position.x, position.y, { disableSafetyCheck: true }).catch(() => { /** Suppress warnings */ })
+                        }
+                    }
+                    if (mage.canUse("attack")) mage.basicAttack(slenderID).catch(() => { /** Suppress warnings */ })
+                    if (mage.canUse("burst")) mage.burst(slenderID).catch(() => { /** Suppress warnings */ })
+                }
+                slenderTrilateration = []
+            }
+        } catch (e) {
+            console.error(e)
+        }
+        bot.timeouts.set("attackLoop", setTimeout(async () => { trilaterationLoop() }, 1000))
+    }
+    trilaterationLoop()
 
     async function attackLoop() {
         try {
             if (!bot.socket || bot.socket.disconnected) return
 
             const slenderman = bot.getNearestMonster("slenderman")?.monster
+            if (slenderman) slenderID = slenderman.id
             if (slenderman && AL.Tools.distance(bot, slenderman) <= bot.range) {
                 // NOTE: We are bursting in the move loop, because we can do it really fast there
                 if (bot.canUse("attack")) await bot.basicAttack(slenderman.id).catch(() => { /** Suppress warnings */ })
             } else {
                 await attackTheseTypesMage(bot, toAttack, [], { disableCburst: true, disableEnergize: true })
             }
+
+            if (slenderID && !bot.c.town && bot.canUse("attack")) {
+                await bot.basicAttack(slenderID).catch(() => { /** Suppress errors */ })
+            }
         } catch (e) {
             console.error(e)
         }
-
         bot.timeouts.set("attackLoop", setTimeout(async () => { attackLoop() }, Math.max(10, Math.min(bot.getCooldown("attack"), bot.getCooldown("burst"), bot.getCooldown("cburst")))))
     }
     attackLoop()
@@ -79,6 +139,7 @@ async function startMage(bot: Mage) {
             await goToBankIfFull(bot)
 
             const slenderman = bot.getNearestMonster("slenderman")?.monster
+            if (slenderman) slenderID = slenderman.id
             if (slenderman && AL.Tools.distance(bot, slenderman) > bot.range) {
                 console.log(`Slenderman spotted at ${slenderman.map},${slenderman.x},${slenderman.y} with ${slenderman.hp}/${slenderman.max_hp} HP.`)
                 if (bot.canUse("blink")) {
@@ -205,8 +266,8 @@ async function run() {
                 return
             }
 
-            // Don't change servers if slender is live
-            if (mage1.S?.slenderman && mage1.S.slenderman.live) {
+            // Don't change servers if slender is live, and we haven't spent a lot of time on the server looking for him
+            if (mage1.S?.slenderman && mage1.S.slenderman.live && lastServerChangeTime > (Date.now() - 900_000)) {
                 setTimeout(async () => { serverLoop() }, 1000)
                 return
             }
@@ -214,12 +275,14 @@ async function run() {
             const currentRegion = mage1.server.region
             const currentIdentifier = mage1.server.name
 
-            const targetServer = await getTargetServerFromPlayer(currentRegion, currentIdentifier, partyLeader)
+            const targetServer = getTargetServerFromCurrentServer(currentRegion, currentIdentifier, true)
             if (currentRegion == targetServer[0] && currentIdentifier == targetServer[1]) {
                 // We're already on the correct server
                 setTimeout(async () => { serverLoop() }, 1000)
                 return
             }
+
+            slenderID = undefined
 
             // Change servers to attack this entity
             region = targetServer[0]
