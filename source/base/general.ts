f81e98ac6b7a21a32e72c1914d9d921fffc3fe56
Kent Rasmussen <hyprkookeez@gmail.com>
2021-11-22 07:44:52 +0900

    lolipops are good now

--- source/base/general.ts
+++ source/base/general.ts
@@ -84,7 +84,7 @@ export const ITEMS_TO_BUY: Set<ItemName> = new Set([
     // Gloves
     /*"gloves1",*/ "goldenpowerglove", "handofmidas", "hgloves", "mpxgloves", "mrngloves", "mwgloves", "poker", "powerglove", "vgloves", "wgloves", "xgloves",
     // Good weapons
-    "basher", "bataxe", "bowofthedead", "candycanesword", "carrotsword", "crossbow", "dartgun", "fireblade", "firebow", "firestaff", "firestars", "frostbow", "froststaff", "gbow", "gstaff", "harbringer", "heartwood", "hbow", "hdagger", "merry", "oozingterror", "ornamentstaff", "pinkie", "pmace", "scythe", "snowflakes", "t2bow", "t3bow", /*"throwingstars",*/ "vdagger", "vhammer", "vstaff", "vsword", "wblade",
+    "basher", "bataxe", "bowofthedead", "candycanesword", "carrotsword", "crossbow", "dartgun", "fireblade", "firebow", "firestaff", "firestars", "frostbow", "froststaff", "gbow", "glolipop", "gstaff", "harbringer", "heartwood", "hbow", "hdagger", "merry", "ololipop", "oozingterror", "ornamentstaff", "pinkie", "pmace", "scythe", "snowflakes", "t2bow", "t3bow", /*"throwingstars",*/ "vdagger", "vhammer", "vstaff", "vsword", "wblade",
     // Things we can exchange / craft with
     "ascale", "bfur", "cscale", "electronics", "feather0", "frogt", "goldenegg", "goldingot", "goldnugget", "ink", "leather", "lotusf", "platinumingot", "platinumnugget", "pleather", "snakefang",
     // Things to make xbox
@@ -111,7 +111,7 @@ export const ITEMS_TO_SELL: ItemLevelInfo = {
     // Things that are now obsolete
     "dexring": 2, "intring": 2, "intearring": 2, "strearring": 2, "stramulet": 2,
     // Things in abundance during halloween
-    "glolipop": 2, "gphelmet": 2, "ololipop": 2, "phelmet": 2,
+    "gphelmet": 2, "phelmet": 2,
     // Field generators
     "fieldgen0": 999,
     // Snowballs
