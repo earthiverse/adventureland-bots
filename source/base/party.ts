53978a823c63daec4b03da01d2895754907af77c
Kent Rasmussen <hyprkookeez@gmail.com>
2022-01-10 15:56:59 +0900

    Fix extension

--- source/base/party.ts
+++ source/base/party.ts
@@ -1,4 +1,4 @@
-import { ANNOUNCEMENT_CHARACTERS, LOLWUTPEAR_CHARACTERS, MY_CHARACTERS } from "./general"
+import { ANNOUNCEMENT_CHARACTERS, LOLWUTPEAR_CHARACTERS, MY_CHARACTERS } from "./general.js"
 
 export const partyLeader = "earthWar"
 /** NOTE: This is in order from high -> low priority. If the party becomes full, lower priority members could be kicked to make room for higher priority members. */
