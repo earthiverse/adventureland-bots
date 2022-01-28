3ef09e17ffc693ba51fcc3d680d4356c3930e2ae
Kent Rasmussen <hyprkookeez@gmail.com>
2022-01-28 21:10:02 +0900

    Update ALClient

--- source/tests/preparePathfinding.ts
+++ source/tests/preparePathfinding.ts
@@ -1,9 +1,18 @@
-import AL from "alclient"
+import AL, { Pathfinder } from "alclient"
 
 async function run() {
     // Login and prepare pathfinding
     await Promise.all([AL.Game.loginJSONFile("../../credentials.json"), AL.Game.getGData(true)])
     await AL.Pathfinder.prepare(AL.Game.G)
+
+    const x = 126
+    const y = -413
+    console.log(`${x},${y}: ${Pathfinder.canStand({ map: "main", x: x, y: y })}`)
+    for (const [dX, dY] of [[0, 0], [-10, 0], [10, 0], [0, -10], [0, 10], [-10, -10], [-10, 10], [10, -10], [10, 10]]) {
+        const roundedX = Math.round((x + dX) / 10) * 10
+        const roundedY = Math.round((y + dY) / 10) * 10
+        console.log(`${roundedX},${roundedY}: ${Pathfinder.canStand({ map: "main", x: roundedX, y: roundedY })}`)
+    }
 
     AL.Database.disconnect()
 }
