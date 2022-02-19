7af5663bf9c16114aa50f10d93342f047b1a0fda
Kent Rasmussen <hyprkookeez@gmail.com>
2022-02-20 08:15:01 +0900

    Fix party leader

--- source/base/party.ts
+++ source/base/party.ts
@@ -1,6 +1,6 @@
 import { ANNOUNCEMENT_CHARACTERS, LOLWUTPEAR_CHARACTERS, MY_CHARACTERS } from "./general.js"
 
-export const partyLeader = "earthiverse"
+export const partyLeader = "earthWar"
 /** NOTE: This is in order from high -> low priority. If the party becomes full, lower priority members could be kicked to make room for higher priority members. */
 export const partyMembers = [
     ...MY_CHARACTERS, ...LOLWUTPEAR_CHARACTERS, ...ANNOUNCEMENT_CHARACTERS
