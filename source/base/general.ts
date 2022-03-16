e864258fd0d5d03ed242894b68e4d90dde005d52
Kent Rasmussen <hyprkookeez@gmail.com>
2022-03-16 19:33:22 +0900

    Fix MP regeneration typo

--- source/base/general.ts
+++ source/base/general.ts
@@ -1208,7 +1208,7 @@ export function startHealLoop(bot: Character): void {
                     }
                 } else if (mpRatio < hpRatio) {
                     if (bot.c.town || bot.s.fishing) {
-                        await bot.regenHP()
+                        await bot.regenMP()
                     } else if (missingMP >= 500 && mpot1 !== undefined) {
                         await bot.useMPPot(mpot1)
                     } else if (missingMP >= 300 && mpot0 !== undefined) {
