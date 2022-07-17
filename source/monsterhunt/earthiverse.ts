2fd0b5acdb56b2913dfbbb774205759ffa1a38f1
Kent Rasmussen <hyprkookeez@gmail.com>
2022-07-17 20:35:02 +0900

    Fix bowequipment

--- source/monsterhunt/earthiverse.ts
+++ source/monsterhunt/earthiverse.ts
@@ -1122,7 +1122,7 @@ function prepareRanger(bot: Ranger) {
 function prepareWarrior(bot: Warrior) {
     const aoeEquipment: { [T in SlotType]?: ItemName } = { amulet: "snring", belt: "strbelt", cape: "bcape", chest: "coat1", earring1: "cearring", earring2: "cearring", gloves: "gloves1", helmet: "helmet1", mainhand: "vhammer", offhand: "glolipop", orb: "orbofstr", pants: "pants1", ring1: "strring", ring2: "strring", shoes: "wingedboots" }
     const burnEquipment: { [T in SlotType]?: ItemName } = { ...aoeEquipment, mainhand: "fireblade", offhand: "fireblade" }
-    const bowEquipment: { [T in SlotType]?: ItemName } = { ...aoeEquipment, mainhand: "hbow" }
+    const bowEquipment: { [T in SlotType]?: ItemName } = { ...aoeEquipment, mainhand: "hbow", offhand: undefined }
     const bscorpionSpawn = Pathfinder.locateMonster("bscorpion")[0]
     const strategy: Strategy = {
         defaultTarget: "spider",
