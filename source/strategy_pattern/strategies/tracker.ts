33c0688ef42ccc3fcb755f943415b01d3d7a664d
Kent Rasmussen <hyprkookeez@gmail.com>
2022-09-07 09:13:10 +0900

    Fix tracker statistics

--- source/strategy_pattern/strategies/tracker.ts
+++ source/strategy_pattern/strategies/tracker.ts
@@ -1,14 +1,15 @@
 import AL, { Character } from "alclient"
 import { Loop, LoopName, Strategy } from "../context.js"
 
+export type TrackerStrategyOptions = {
+    /** How often to call to update statistics */
+    interval: number
+}
+
 export class TrackerStrategy implements Strategy<Character> {
     public loops = new Map<LoopName, Loop<Character>>()
 
-    public constructor(options = { interval: 900_000 }) {
-        // This strategy is only useful if we have a database connection, because the only reason we're calling
-        // `tracker` is to update the database with achievement progress.
-        if (!AL.Database.connection) return
-
+    public constructor(options: TrackerStrategyOptions = { interval: 900_000 }) {
         this.loops.set("tracker", {
             fn: async (bot: Character) => { await this.checkTracker(bot) },
             interval: options?.interval ?? 900_000
@@ -16,6 +17,10 @@ export class TrackerStrategy implements Strategy<Character> {
     }
 
     private async checkTracker(bot: Character) {
+        // This strategy is only useful if we have a database connection, because the only reason we're calling
+        // `tracker` is to update the database with achievement progress.
+        if (!AL.Database.connection) return
+
         if ((bot.hasItem("tracker") || bot.hasItem("supercomputer")) && bot.cc < 100) {
             await bot.getTrackerData()
         } else {
