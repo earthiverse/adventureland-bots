9e227b691f318b04009bbf2d874b59aa953908ba
Kent Rasmussen <hyprkookeez@gmail.com>
2022-09-28 19:19:59 +0900

    Update priest equipment

--- source/strategy_pattern/setups/equipment.ts
+++ source/strategy_pattern/setups/equipment.ts
@@ -1,10 +1,10 @@
 import { EnsureEquipped } from "../strategies/attack"
 
 export const PRIEST_ARMOR: EnsureEquipped = {
-    chest: { name: "harmor", filters: { returnHighestLevel: true } },
+    chest: { name: "vattire", filters: { returnHighestLevel: true } },
     earring1: { name: "cearring", filters: { returnHighestLevel: true } },
     earring2: { name: "cearring", filters: { returnHighestLevel: true } },
-    gloves: { name: "hgloves", filters: { returnHighestLevel: true } },
+    gloves: { name: "vgloves", filters: { returnHighestLevel: true } },
     helmet: { name: "hhelmet", filters: { returnHighestLevel: true } },
     mainhand: { name: "firestaff", filters: { returnHighestLevel: true } },
     offhand: { name: "tigershield", filters: { returnHighestLevel: true } },
