8ee96cbd0e8e9fb886e08e3f63282d78e0e4ade5
Kent Rasmussen <hyprkookeez@gmail.com>
2022-11-09 08:35:41 +0900

    Add another setup for bbpompom

--- source/strategy_pattern/setups/bbpompom.ts
+++ source/strategy_pattern/setups/bbpompom.ts
@@ -2,9 +2,10 @@ import { PingCompensatedCharacter } from "alclient"
 import { Strategist } from "../context.js"
 import { MageAttackStrategy } from "../strategies/attack_mage.js"
 import { PriestAttackStrategy } from "../strategies/attack_priest.js"
+import { WarriorAttackStrategy } from "../strategies/attack_warrior.js"
 import { ImprovedMoveStrategy } from "../strategies/move.js"
 import { Setup } from "./base"
-import { PRIEST_LUCK } from "./equipment.js"
+import { MAGE_SPLASH, PRIEST_LUCK, WARRIOR_SPLASH } from "./equipment.js"
 
 export function constructBBPomPomSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
     return {
@@ -17,10 +18,33 @@ export function constructBBPomPomSetup(contexts: Strategist<PingCompensatedChara
                         attack: new MageAttackStrategy({
                             contexts: contexts,
                             disableEnergize: true,
-                            ensureEquipped: {
-                                mainhand: { name: "firestaff", filters: { returnHighestLevel: true } },
-                                offhand: { name: "wbook1", filters: { returnHighestLevel: true } }
-                            },
+                            ensureEquipped: { ...MAGE_SPLASH },
+                            type: "bbpompom"
+                        }),
+                        move: new ImprovedMoveStrategy("bbpompom")
+                    },
+                    {
+                        ctype: "priest",
+                        attack: new PriestAttackStrategy({
+                            contexts: contexts,
+                            disableEnergize: true,
+                            enableGreedyAggro: true,
+                            ensureEquipped: { ...PRIEST_LUCK },
+                            type: "bbpompom",
+                        }),
+                        move: new ImprovedMoveStrategy("bbpompom")
+                    }
+                ]
+            },
+            {
+                id: "bbpompom_warrior,priest",
+                characters: [
+                    {
+                        ctype: "warrior",
+                        attack: new WarriorAttackStrategy({
+                            contexts: contexts,
+                            enableEquipForCleave: true,
+                            ensureEquipped: { ...WARRIOR_SPLASH },
                             type: "bbpompom"
                         }),
                         move: new ImprovedMoveStrategy("bbpompom")
