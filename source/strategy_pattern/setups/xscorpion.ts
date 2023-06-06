4d9bb0e3a27cf9ca40528b0433e095e8be8c44b2
Kent Rasmussen <hyprkookeez@gmail.com>
2023-06-06 15:46:25 +0900

    Improve xscorpions

--- source/strategy_pattern/setups/xscorpion.ts
+++ source/strategy_pattern/setups/xscorpion.ts
@@ -3,13 +3,16 @@ import { Strategist } from "../context.js"
 import { MageAttackStrategy } from "../strategies/attack_mage.js"
 import { PriestAttackStrategy } from "../strategies/attack_priest.js"
 import { WarriorAttackStrategy } from "../strategies/attack_warrior.js"
-import { HoldPositionMoveStrategy, MoveInCircleMoveStrategy } from "../strategies/move.js"
+import { HoldPositionMoveStrategy, ImprovedMoveStrategy, MoveInCircleMoveStrategy } from "../strategies/move.js"
 import { Setup } from "./base"
-import { MAGE_SPLASH, PRIEST_ARMOR, WARRIOR_SPLASH } from "./equipment.js"
+import { MAGE_ARMOR, MAGE_SPLASH, PRIEST_ARMOR, WARRIOR_SPLASH } from "./equipment.js"
 
 export function constructXScorpionSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
     const spawn = AL.Pathfinder.locateMonster("xscorpion")[0]
     const monsters: MonsterName[] = ["xscorpion", "tinyp"]
+
+    const moveInCircleStrategy = new MoveInCircleMoveStrategy({ center: spawn, radius: 20, sides: 8 })
+    const moveStrategy = new ImprovedMoveStrategy("xscorpion")
 
     return {
         configs: [
@@ -46,11 +49,64 @@ export function constructXScorpionSetup(contexts: Strategist<PingCompensatedChar
                             enableEquipForCleave: true,
                             enableGreedyAggro: true,
                             ensureEquipped: { ...WARRIOR_SPLASH },
+                            typeList: monsters,
+                        }),
+                        move: moveInCircleStrategy
+                    }
+                ]
+            },
+            {
+                id: "greedy_xscorpion_priest,warrior",
+                characters: [
+                    {
+                        ctype: "priest",
+                        attack: new PriestAttackStrategy({
+                            contexts: contexts,
+                            disableEnergize: true,
+                            ensureEquipped: { ...PRIEST_ARMOR },
+                            typeList: monsters,
+                        }),
+                        move: new HoldPositionMoveStrategy(spawn)
+                    },
+                    {
+                        ctype: "warrior",
+                        attack: new WarriorAttackStrategy({
+                            contexts: contexts,
+                            disableZapper: true,
+                            enableEquipForCleave: true,
+                            enableGreedyAggro: true,
+                            ensureEquipped: { ...WARRIOR_SPLASH },
+                            typeList: monsters,
+                        }),
+                        move: moveInCircleStrategy
+                    }
+                ]
+            },
+            {
+                id: "xscorpion_mage,priest",
+                characters: [
+                    {
+                        ctype: "mage",
+                        attack: new MageAttackStrategy({
+                            contexts: contexts,
+                            disableEnergize: true,
+                            disableZapper: true,
+                            ensureEquipped: { ...MAGE_ARMOR },
                             targetingPartyMember: true,
                             typeList: monsters,
                         }),
-                        move: new MoveInCircleMoveStrategy({ center: spawn, radius: 20, sides: 8 })
-                    }
+                        move: moveStrategy
+                    },
+                    {
+                        ctype: "priest",
+                        attack: new PriestAttackStrategy({
+                            contexts: contexts,
+                            disableEnergize: true,
+                            ensureEquipped: { ...PRIEST_ARMOR },
+                            typeList: monsters,
+                        }),
+                        move: moveStrategy
+                    },
                 ]
             },
         ]
