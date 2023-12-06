a882e63d7ce3a601dfcba1615e74653b251fd46d
Kent Rasmussen <hyprkookeez@gmail.com>
2023-12-06 10:24:18 +0900

    improve destroy for merchants

--- source/strategy_pattern/strategies/destroy.ts
+++ source/strategy_pattern/strategies/destroy.ts
@@ -1,4 +1,4 @@
-import { ItemName, Character } from "alclient"
+import AL, { ItemName, Character } from "alclient"
 import { Loop, LoopName, Strategy } from "../context.js"
 
 export type DestroyStrategyOptions = {
@@ -32,6 +32,16 @@ export class DestroyStrategy<Type extends Character> implements Strategy<Type> {
             if (!item) continue
             if (item.level !== 0) continue
             if (!this.options.destroy.has(item.name)) continue
+
+            if (bot.ctype === "merchant") {
+                // Prevent destroying items needed to craft rod or pickaxe if we don't have one
+                if (!bot.hasItem("rod") && !bot.isEquipped("rod")) {
+                    if (AL.Game.G.craft.rod.items.some((a) => a[1] === item.name)) continue
+                }
+                if (!bot.hasItem("pickaxe") && !bot.isEquipped("pickaxe")) {
+                    if (AL.Game.G.craft.pickaxe.items.some((a) => a[1] === item.name)) continue
+                }
+            }
 
             // @ts-ignore TODO: Move to ALClient
             bot.socket.emit("destroy", { num: i, q: 1, statue: true })
