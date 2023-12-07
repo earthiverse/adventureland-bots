7d2b45e95a7fd1742c81b1ca1693eb36e9b1ba49
Kent Rasmussen <hyprkookeez@gmail.com>
2023-12-08 08:10:21 +0900

    fix server urls

--- source/earthiverse_battleau.ts
+++ source/earthiverse_battleau.ts
@@ -1,4 +1,4 @@
-import AL, { ItemName, MonsterName, ServerRegion } from "alclient"
+import AL, { ItemName, MonsterName, ServerData, ServerRegion } from "alclient"
 import { RunnerOptions, startCharacterFromName, startRunner } from "./strategy_pattern/runner.js"
 import { DEFAULT_ITEMS_TO_HOLD } from "./base/defaults.js"
 
@@ -9,6 +9,13 @@ AL.Game.setServer("https://battle.au")
 
 await Promise.all([AL.Game.loginJSONFile(CREDENTIALS, true), AL.Game.getGData(false)])
 await AL.Pathfinder.prepare(AL.Game.G, { cheat: false })
+
+// Hack to fix URLs
+for(const region in AL.Game.servers) {
+    for(const id in AL.Game.servers[region]) {
+        AL.Game.servers[region][id].addr = AL.Game.servers[region][id].addr.replace('aud1.', '')
+    }
+}
 
 // Add a whole bunch of items to the sell list
 const SELL_MAP: Map<ItemName, [number, number][]> = new Map([
@@ -35,9 +42,9 @@ const options: RunnerOptions = {
 }
 
 for (const character of ["earthiverse", "earthMag", "earthPri", "earthMer"]) {
-    startRunner(await startCharacterFromName(character, "US", "I"), options)
+    startRunner(await AL.Game.startCharacter(character, "US", "I"), options)
 }
 
 for (const character of ["earthRog"]) {
-    startRunner(await startCharacterFromName(character, "AU" as ServerRegion, "I"), options)
+    startRunner(await AL.Game.startCharacter(character, "AU" as ServerRegion, "I"), options)
 }
