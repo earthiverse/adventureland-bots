d0b7fdf94024dd0af49a90ceb619774743e24187
Kent Rasmussen <hyprkookeez@gmail.com>
2024-07-31 14:20:30 +0900

    improve mole strategy, refactor harpy strategy

--- source/strategy_pattern/setups/harpy.ts
+++ source/strategy_pattern/setups/harpy.ts
@@ -4,121 +4,69 @@ import { MageAttackStrategy } from "../strategies/attack_mage.js"
 import { PriestAttackStrategy } from "../strategies/attack_priest.js"
 import { WarriorAttackStrategy } from "../strategies/attack_warrior.js"
 import { SpecialMonsterMoveStrategy } from "../strategies/move.js"
-import { Setup } from "./base"
+import { CharacterConfig, Setup } from "./base"
 
 export function constructHarpySetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
-    const moveStrategy = new SpecialMonsterMoveStrategy({ contexts: contexts, disableCheckDB: true, typeList: ["harpy"] })
+    const moveStrategy = new SpecialMonsterMoveStrategy({
+        contexts: contexts,
+        disableCheckDB: true,
+        typeList: ["harpy"],
+    })
+    const mageConfig: CharacterConfig = {
+        ctype: "mage",
+        attack: new MageAttackStrategy({
+            contexts: contexts,
+            disableEnergize: true,
+            generateEnsureEquipped: {
+                attributes: ["armor", "int", "attack"],
+            },
+            maximumTargets: 1,
+            type: "harpy",
+        }),
+        move: moveStrategy,
+    }
+    const priestConfig: CharacterConfig = {
+        ctype: "priest",
+        attack: new PriestAttackStrategy({
+            contexts: contexts,
+            disableEnergize: true,
+            generateEnsureEquipped: {
+                attributes: ["armor", "int", "attack"],
+            },
+            maximumTargets: 1,
+            type: "harpy",
+        }),
+        move: moveStrategy,
+    }
+    const warriorConfig: CharacterConfig = {
+        ctype: "warrior",
+        attack: new WarriorAttackStrategy({
+            contexts: contexts,
+            enableEquipForCleave: true,
+            enableEquipForStomp: true,
+            generateEnsureEquipped: {
+                attributes: ["armor", "str", "attack"],
+            },
+            maximumTargets: 1,
+            type: "harpy",
+        }),
+        move: moveStrategy,
+    }
 
     return {
         configs: [
             {
                 id: "harpy_mage,priest,warrior",
-                characters: [
-                    {
-                        ctype: "mage",
-                        attack: new MageAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "int", "attack"]
-                            },
-                            maximumTargets: 1,
-                            type: "harpy",
-                        }),
-                        move: moveStrategy
-                    },
-                    {
-                        ctype: "priest",
-                        attack: new PriestAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "int", "attack"]
-                            },
-                            maximumTargets: 1,
-                            type: "harpy",
-                        }),
-                        move: moveStrategy
-                    },
-                    {
-                        ctype: "warrior",
-                        attack: new WarriorAttackStrategy({
-                            contexts: contexts,
-                            enableEquipForCleave: true,
-                            enableEquipForStomp: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "str", "attack"]
-                            },
-                            maximumTargets: 1,
-                            type: "harpy",
-                        }),
-                        move: moveStrategy
-                    }
-                ]
+                characters: [mageConfig, priestConfig, warriorConfig],
             },
             {
                 id: "harpy_mage,priest",
-                characters: [
-                    {
-                        ctype: "mage",
-                        attack: new MageAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "int", "attack"]
-                            },
-                            maximumTargets: 1,
-                            type: "harpy",
-                        }),
-                        move: moveStrategy
-                    },
-                    {
-                        ctype: "priest",
-                        attack: new PriestAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "int", "attack"]
-                            },
-                            maximumTargets: 1,
-                            type: "harpy",
-                        }),
-                        move: moveStrategy
-                    },
-                ]
+                characters: [mageConfig, priestConfig],
             },
             {
                 id: "harpy_priest,warrior",
-                characters: [
-                    {
-                        ctype: "priest",
-                        attack: new PriestAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "int", "attack"]
-                            },
-                            maximumTargets: 1,
-                            type: "harpy",
-                        }),
-                        move: moveStrategy
-                    },
-                    {
-                        ctype: "warrior",
-                        attack: new WarriorAttackStrategy({
-                            contexts: contexts,
-                            enableEquipForCleave: true,
-                            enableEquipForStomp: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "str", "attack"]
-                            },
-                            maximumTargets: 1,
-                            type: "harpy",
-                        }),
-                        move: moveStrategy
-                    }
-                ]
+                characters: [priestConfig, warriorConfig],
             },
-        ]
+        ],
     }
 }
--- source/strategy_pattern/setups/mole.ts
+++ source/strategy_pattern/setups/mole.ts
@@ -2,54 +2,72 @@ import { PingCompensatedCharacter } from "alclient"
 import { Strategist } from "../context.js"
 import { PriestAttackStrategy } from "../strategies/attack_priest.js"
 import { WarriorAttackStrategy } from "../strategies/attack_warrior.js"
-import { ImprovedMoveStrategy } from "../strategies/move.js"
-import { Setup } from "./base"
+import { ImprovedMoveStrategy, KiteMonsterMoveStrategy } from "../strategies/move.js"
+import { CharacterConfig, Setup } from "./base"
 import { RETURN_HIGHEST } from "./equipment.js"
+import { RangerAttackStrategy } from "../strategies/attack_ranger.js"
 
 export function constructMoleSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
     const moveStrategy = new ImprovedMoveStrategy("mole", {
         idlePosition: {
             map: "tunnel",
             x: 0,
-            y: -735
-        }
+            y: -735,
+        },
     })
+    const priestConfig: CharacterConfig = {
+        ctype: "priest",
+        attack: new PriestAttackStrategy({
+            contexts: contexts,
+            disableEnergize: true,
+            generateEnsureEquipped: {
+                attributes: ["armor", "int", "attack"],
+            },
+            type: "mole",
+        }),
+        move: moveStrategy,
+    }
+    const warriorConfig: CharacterConfig = {
+        ctype: "warrior",
+        attack: new WarriorAttackStrategy({
+            contexts: contexts,
+            enableEquipForCleave: true,
+            generateEnsureEquipped: {
+                attributes: ["armor", "str", "blast", "explosion"],
+                prefer: {
+                    ring1: { name: "zapper", filters: RETURN_HIGHEST },
+                },
+            },
+            enableGreedyAggro: true,
+            type: "mole",
+        }),
+        move: moveStrategy,
+    }
+    // Support ranger shouldn't tank moles
+    const supportRangerConfig: CharacterConfig = {
+        ctype: "ranger",
+        attack: new RangerAttackStrategy({
+            contexts: contexts,
+            generateEnsureEquipped: {
+                attributes: ["armor"],
+            },
+            hasTarget: true,
+            maximumTargets: 0,
+            type: "mole",
+        }),
+        move: new KiteMonsterMoveStrategy({ contexts: contexts, typeList: ["mole"] }),
+    }
 
     return {
         configs: [
             {
-                id: "mole_priest,warrior",
-                characters: [
-                    {
-                        ctype: "priest",
-                        attack: new PriestAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "int", "attack"]
-                            },
-                            type: "mole",
-                        }),
-                        move: moveStrategy
-                    },
-                    {
-                        ctype: "warrior",
-                        attack: new WarriorAttackStrategy({
-                            contexts: contexts,
-                            enableEquipForCleave: true,
-                            generateEnsureEquipped: {
-                                attributes: ["armor", "str", "blast", "explosion"],
-                                prefer: {
-                                    ring1: { name: "zapper", filters: RETURN_HIGHEST },
-                                }
-                            },
-                            enableGreedyAggro: true,
-                            type: "mole"
-                        }),
-                        move: moveStrategy
-                    }
-                ]
+                id: "mole_priest,warrior,ranger",
+                characters: [priestConfig, warriorConfig, supportRangerConfig],
             },
-        ]
+            {
+                id: "mole_priest,warrior",
+                characters: [priestConfig, warriorConfig],
+            },
+        ],
     }
 }
