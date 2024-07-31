e125e840414d20cad39651e69d61fcfdef912805
Kent Rasmussen <hyprkookeez@gmail.com>
2024-07-31 13:50:34 +0900

    add triple mage phoenix setup

--- source/strategy_pattern/setups/phoenix.ts
+++ source/strategy_pattern/setups/phoenix.ts
@@ -7,123 +7,96 @@ import { RangerAttackStrategy } from "../strategies/attack_ranger.js"
 import { RogueAttackStrategy } from "../strategies/attack_rogue.js"
 import { WarriorAttackStrategy } from "../strategies/attack_warrior.js"
 import { SpecialMonsterMoveStrategy } from "../strategies/move.js"
-import { Setup } from "./base"
+import { CharacterConfig, Setup } from "./base"
+import { RETURN_HIGHEST } from "./equipment.js"
 
 const typeList: MonsterName[] = ["phoenix"]
 
 export function constructPhoenixSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
     const phoenixMoveStrategy = new SpecialMonsterMoveStrategy({ contexts: contexts, typeList: typeList })
 
+    const mageConfig: CharacterConfig = {
+        ctype: "mage",
+        attack: new MageAttackStrategy({
+            contexts: contexts,
+            generateEnsureEquipped: {
+                attributes: ["attack", "luck", "int"],
+                prefer: {
+                    mainhand: { name: "firestaff", filters: RETURN_HIGHEST },
+                },
+            },
+            typeList: typeList,
+        }),
+        move: phoenixMoveStrategy,
+    }
+    const rogueConfig: CharacterConfig = {
+        ctype: "rogue",
+        attack: new RogueAttackStrategy({
+            contexts: contexts,
+            generateEnsureEquipped: {
+                attributes: ["attack", "luck", "dex"],
+            },
+            typeList: typeList,
+        }),
+        move: phoenixMoveStrategy,
+    }
+    const priestConfig: CharacterConfig = {
+        ctype: "priest",
+        attack: new PriestAttackStrategy({
+            contexts: contexts,
+            disableEnergize: true,
+            enableGreedyAggro: true,
+            generateEnsureEquipped: {
+                attributes: ["attack", "luck", "int"],
+                prefer: {
+                    mainhand: { name: "firestaff", filters: RETURN_HIGHEST },
+                },
+            },
+            typeList: typeList,
+        }),
+        move: phoenixMoveStrategy,
+    }
+    const warriorConfig: CharacterConfig = {
+        ctype: "warrior",
+        attack: new WarriorAttackStrategy({
+            contexts: contexts,
+            enableEquipForCleave: true,
+            enableEquipForStomp: true,
+            enableGreedyAggro: true,
+            generateEnsureEquipped: {
+                attributes: ["attack", "luck", "str"],
+                prefer: {
+                    mainhand: { name: "fireblade", filters: RETURN_HIGHEST },
+                },
+            },
+            typeList: typeList,
+        }),
+        move: phoenixMoveStrategy,
+    }
+
     return {
         configs: [
             {
+                id: "phoenix_triple_mage",
+                characters: [mageConfig, mageConfig, mageConfig],
+            },
+            {
                 id: "phoenix_mage,priest,warrior",
-                characters: [
-                    {
-                        ctype: "mage",
-                        attack: new MageAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            generateEnsureEquipped: {
-                                attributes: ["attack", "luck", "int"]
-                            },
-                            typeList: typeList
-                        }),
-                        move: phoenixMoveStrategy
-                    },
-                    {
-                        ctype: "priest",
-                        attack: new PriestAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            enableGreedyAggro: true,
-                            generateEnsureEquipped: {
-                                attributes: ["attack", "luck", "int"]
-                            },
-                            typeList: typeList,
-                        }),
-                        move: phoenixMoveStrategy
-                    },
-                    {
-                        ctype: "warrior",
-                        attack: new WarriorAttackStrategy({
-                            contexts: contexts,
-                            enableEquipForCleave: true,
-                            enableEquipForStomp: true,
-                            enableGreedyAggro: true,
-                            generateEnsureEquipped: {
-                                attributes: ["attack", "luck", "str"]
-                            },
-                            typeList: typeList
-                        }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
+                characters: [mageConfig, priestConfig, warriorConfig],
             },
             {
                 id: "phoenix_priest,warrior",
-                characters: [
-                    {
-                        ctype: "priest",
-                        attack: new PriestAttackStrategy({
-                            contexts: contexts,
-                            disableEnergize: true,
-                            enableGreedyAggro: true,
-                            generateEnsureEquipped: {
-                                attributes: ["attack", "luck", "int"]
-                            },
-                            typeList: typeList,
-                        }),
-                        move: phoenixMoveStrategy
-                    },
-                    {
-                        ctype: "warrior",
-                        attack: new WarriorAttackStrategy({
-                            contexts: contexts,
-                            enableEquipForCleave: true,
-                            enableGreedyAggro: true,
-                            generateEnsureEquipped: {
-                                attributes: ["attack", "luck", "str"]
-                            },
-                            typeList: typeList
-                        }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
+                characters: [priestConfig, warriorConfig],
             },
             {
                 id: "phoenix_mage",
-                characters: [
-                    {
-                        ctype: "mage",
-                        attack: new MageAttackStrategy({
-                            contexts: contexts,
-                            generateEnsureEquipped: {
-                                attributes: ["attack", "luck", "int"]
-                            },
-                            typeList: typeList,
-                        }),
-                        move: phoenixMoveStrategy
-                    },
-                ]
+                characters: [mageConfig],
             },
             {
                 id: "phoenix_rogue",
-                characters: [
-                    {
-                        ctype: "rogue",
-                        attack: new RogueAttackStrategy({
-                            contexts: contexts,
-                            generateEnsureEquipped: {
-                                attributes: ["attack", "luck", "dex"]
-                            },
-                            typeList: typeList,
-                        }),
-                        move: phoenixMoveStrategy
-                    },
-                ]
+                characters: [rogueConfig],
             },
-        ]
+        ],
     }
 }
 
@@ -139,11 +112,11 @@ export function constructPhoenixHelperSetup(contexts: Strategist<PingCompensated
                         ctype: "mage",
                         attack: new MageAttackStrategy({
                             contexts: contexts,
-                            typeList: typeList
+                            typeList: typeList,
                         }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
+                        move: phoenixMoveStrategy,
+                    },
+                ],
             },
             {
                 id: "phoenix_helper_paladin",
@@ -152,11 +125,11 @@ export function constructPhoenixHelperSetup(contexts: Strategist<PingCompensated
                         ctype: "paladin",
                         attack: new PaladinAttackStrategy({
                             contexts: contexts,
-                            typeList: typeList
+                            typeList: typeList,
                         }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
+                        move: phoenixMoveStrategy,
+                    },
+                ],
             },
             {
                 id: "phoenix_helper_priest",
@@ -166,11 +139,11 @@ export function constructPhoenixHelperSetup(contexts: Strategist<PingCompensated
                         attack: new PriestAttackStrategy({
                             contexts: contexts,
                             disableAbsorb: true,
-                            typeList: typeList
+                            typeList: typeList,
                         }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
+                        move: phoenixMoveStrategy,
+                    },
+                ],
             },
             {
                 id: "phoenix_helper_ranger",
@@ -179,11 +152,11 @@ export function constructPhoenixHelperSetup(contexts: Strategist<PingCompensated
                         ctype: "ranger",
                         attack: new RangerAttackStrategy({
                             contexts: contexts,
-                            typeList: typeList
+                            typeList: typeList,
                         }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
+                        move: phoenixMoveStrategy,
+                    },
+                ],
             },
             {
                 id: "phoenix_helper_rogue",
@@ -192,11 +165,11 @@ export function constructPhoenixHelperSetup(contexts: Strategist<PingCompensated
                         ctype: "rogue",
                         attack: new RogueAttackStrategy({
                             contexts: contexts,
-                            typeList: typeList
+                            typeList: typeList,
                         }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
+                        move: phoenixMoveStrategy,
+                    },
+                ],
             },
             {
                 id: "phoenix_helper_warrior",
@@ -208,12 +181,12 @@ export function constructPhoenixHelperSetup(contexts: Strategist<PingCompensated
                             disableAgitate: true,
                             enableEquipForCleave: true,
                             enableEquipForStomp: true,
-                            typeList: typeList
+                            typeList: typeList,
                         }),
-                        move: phoenixMoveStrategy
-                    }
-                ]
-            }
-        ]
+                        move: phoenixMoveStrategy,
+                    },
+                ],
+            },
+        ],
     }
 }
