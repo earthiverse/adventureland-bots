6268cb15513a888d639fcb5c2417e2d3f4498bde
Kent Rasmussen <hyprkookeez@gmail.com>
2024-10-06 17:28:36 +0900

    improve logging

--- src/utilities/logging.ts
+++ src/utilities/logging.ts
@@ -1,4 +1,7 @@
 import config from "config";
+import fs from "fs";
+import path from "path";
+import url from "url";
 
 /**
  * Levels correspond to https://datatracker.ietf.org/doc/html/rfc5424
@@ -14,11 +17,34 @@ export enum Level {
   Debug = 7,
 }
 
-export type Method = "console" | "ignore";
+const LEVEL_MAP: Record<Level, string> = {
+  "0": "Emergency",
+  "1": "Alert",
+  "2": "Critical",
+  "3": "Error",
+  "4": "Warning",
+  "5": "Notice",
+  "6": "Informational",
+  "7": "Debug",
+};
+
+export type Method = "console" | "data" | "ignore";
 
 const LOG_MAP = config.get<{ [T in Level]: Method }>("logging.map");
 
-function log(message: string | Error, level: Level = Level.Informational): void {
+export function writeLogToData(message: string | Error, level: Level) {
+  // Make sure the folder exists
+  const folder = path.join(path.dirname(url.fileURLToPath(import.meta.url)), "../data/log/");
+  fs.mkdirSync(folder, { recursive: true });
+
+  // Append to logs with the filename as the date
+  const now = new Date().toISOString();
+  const day = now.slice(0, 10);
+  const time = now.slice(11, 19);
+  fs.appendFileSync(path.join(folder, day), `${time} ${LEVEL_MAP[level]} ${message}`);
+}
+
+export function log(message: string | Error, level: Level): void {
   const method: Method = LOG_MAP[level] ?? "ignore";
   if (method === "ignore") return; // Ignoring
 
@@ -30,6 +56,7 @@ function log(message: string | Error, level: Level = Level.Informational): void 
       switch (level) {
         case Level.Debug:
           return console.debug(message);
+        default:
         case Level.Informational:
         case Level.Notice:
           return console.log(message);
@@ -41,7 +68,33 @@ function log(message: string | Error, level: Level = Level.Informational): void 
         case Level.Emergency:
           return console.error(message);
       }
+    case "data":
+      return writeLogToData(message, level);
   }
+}
+
+export function logEmergency(message: string | Error): void {
+  return log(message, Level.Emergency);
+}
+
+export function logAlert(message: string | Error): void {
+  return log(message, Level.Alert);
+}
+
+export function logCritical(message: string | Error): void {
+  return log(message, Level.Critical);
+}
+
+export function logError(message: string | Error): void {
+  return log(message, Level.Error);
+}
+
+export function logWarning(message: string | Error): void {
+  return log(message, Level.Warning);
+}
+
+export function logNotice(message: string | Error): void {
+  return log(message, Level.Notice);
 }
 
 export function logInformational(message: string | Error): void {
