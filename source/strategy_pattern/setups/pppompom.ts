671b9bea4b98e8ef4664c42ca399d8ee34bd8d4b
Kent Rasmussen <hyprkookeez@gmail.com>
2025-02-08 22:32:41 +0900

    improve ppompom strategy

--- source/strategy_pattern/setups/pppompom.ts
+++ source/strategy_pattern/setups/pppompom.ts
@@ -5,7 +5,7 @@ import { PriestAttackStrategy } from "../strategies/attack_priest.js"
 import { WarriorAttackStrategy } from "../strategies/attack_warrior.js"
 import { HoldPositionMoveStrategy } from "../strategies/move.js"
 import { Setup } from "./base"
-import { ZAPPER_CRING, ZAPPER_STRRING } from "./equipment.js"
+import { RETURN_HIGHEST, ZAPPER_CRING, ZAPPER_STRRING } from "./equipment.js"
 
 export function constructPPPomPomSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
     return {
@@ -20,13 +20,16 @@ export function constructPPPomPomSetup(contexts: Strategist<PingCompensatedChara
                             disableEnergize: true,
                             generateEnsureEquipped: {
                                 attributes: ["armor", "int", "attack"],
-                                prefer: ZAPPER_CRING
+                                prefer: {
+                                    ...ZAPPER_CRING,
+                                    mainhand: { name: "firestaff", filters: RETURN_HIGHEST },
+                                },
                             },
                             maximumTargets: 1,
                             targetingPartyMember: true,
-                            type: "pppompom"
+                            type: "pppompom",
                         }),
-                        move: new HoldPositionMoveStrategy({ map: "level2n", x: 120, y: -170 })
+                        move: new HoldPositionMoveStrategy({ map: "level2n", x: 120, y: -170 }),
                     },
                     {
                         ctype: "priest",
@@ -36,12 +39,15 @@ export function constructPPPomPomSetup(contexts: Strategist<PingCompensatedChara
                             enableAbsorbToTank: true,
                             generateEnsureEquipped: {
                                 attributes: ["armor", "int", "attack"],
-                                prefer: ZAPPER_CRING
+                                prefer: {
+                                    ...ZAPPER_CRING,
+                                    mainhand: { name: "firestaff", filters: RETURN_HIGHEST },
+                                },
                             },
                             maximumTargets: 1,
                             type: "pppompom",
                         }),
-                        move: new HoldPositionMoveStrategy({ map: "level2n", x: 120, y: -130 })
+                        move: new HoldPositionMoveStrategy({ map: "level2n", x: 120, y: -130 }),
                     },
                     {
                         ctype: "warrior",
@@ -49,16 +55,20 @@ export function constructPPPomPomSetup(contexts: Strategist<PingCompensatedChara
                             contexts: contexts,
                             generateEnsureEquipped: {
                                 attributes: ["armor", "str", "attack"],
-                                prefer: ZAPPER_STRRING
+                                prefer: {
+                                    ...ZAPPER_STRRING,
+                                    mainhand: { name: "fireblade", filters: RETURN_HIGHEST },
+                                    offhand: { name: "fireblade", filters: RETURN_HIGHEST },
+                                },
                             },
                             maximumTargets: 1,
                             targetingPartyMember: true,
-                            type: "pppompom"
+                            type: "pppompom",
                         }),
-                        move: new HoldPositionMoveStrategy({ map: "level2n", x: 120, y: -150 })
-                    }
-                ]
+                        move: new HoldPositionMoveStrategy({ map: "level2n", x: 120, y: -150 }),
+                    },
+                ],
             },
-        ]
+        ],
     }
 }
