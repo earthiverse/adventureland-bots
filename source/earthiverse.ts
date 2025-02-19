01c8612608272f362c58adc910392d8946dab2d2
Kent Rasmussen <hyprkookeez@gmail.com>
2025-02-19 21:22:27 +0900

    go back to farming boars

--- source/earthiverse.ts
+++ source/earthiverse.ts
@@ -76,11 +76,11 @@ await AL.Pathfinder.prepare(AL.Game.G, { remove_abtesting: true, remove_test: tr
 
 // TODO: Make these configurable through /comm using a similar system to how lulz works
 // Toggles
-const ENABLE_EVENTS = true
-const ENABLE_SERVER_HOPS = true
-const ENABLE_SPECIAL_MONSTERS = true
-let ENABLE_MONSTERHUNTS = true
-const DEFAULT_MONSTERS: MonsterName[] = ["squigtoad", "squig"]
+const ENABLE_EVENTS = false
+const ENABLE_SERVER_HOPS = false
+const ENABLE_SPECIAL_MONSTERS = false
+let ENABLE_MONSTERHUNTS = false
+const DEFAULT_MONSTERS: MonsterName[] = ["boar"]
 const SPECIAL_MONSTERS: MonsterName[] = [
     "crabxx",
     "cutebee",
@@ -101,16 +101,15 @@ const SPECIAL_MONSTERS: MonsterName[] = [
 const MAX_PUBLIC_CHARACTERS = 6
 
 const MERCHANT: string = "earthMer" // earthMer, earthMer2, earthMer3
-const WARRIORS: string[] = ["earthWar"] // earthWar, earthWar2, earthWar3
-const MAGES: string[] = ["earthMag"] // earthMag, earthMag2, earthMag3
-const PRIESTS: string[] = ["earthPri"] // earthPri, earthPri2
-const RANGERS: string[] = [] // earthiverse, earthRan2, earthRan3
+const WARRIORS: string[] = [] // earthWar, earthWar2, earthWar3
+const MAGES: string[] = [] // earthMag, earthMag2, earthMag3
+const PRIESTS: string[] = [] // earthPri, earthPri2
+const RANGERS: string[] = ["earthiverse", "earthRan2", "earthRan3"] // earthiverse, earthRan2, earthRan3
 const PALADINS: string[] = [] // earthPal
 const ROGUES: string[] = [] // earthRog, earthRog2, earthRog3
 
-const PARTY_LEADER: string = WARRIORS[0]
-
 const PARTY_ALLOWLIST: string[] = [...RANGERS, ...MAGES, ...PRIESTS, ...WARRIORS, ...PALADINS, ...ROGUES]
+const PARTY_LEADER: string = PARTY_ALLOWLIST[0]
 
 // Sanity checks
 if (PARTY_LEADER === undefined) throw new Error("We don't have a party leader set!")
