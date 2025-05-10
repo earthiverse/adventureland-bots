7c36410880576d1c5cc0049f6610f3e42682b781
Kent Rasmussen <hyprkookeez@gmail.com>
2025-05-10 20:15:19 +0900

    update packages

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
-    ignores: ["data/**/*", "dist/**/*"],
+    ignores: ["coverage/**/*", "data/**/*", "dist/**/*"],
   },
   {
     languageOptions: {
@@ -19,6 +19,7 @@ export default tseslint.config(
   },
   {
     rules: {
+      "@typescript-eslint/consistent-type-imports": "error",
       "@typescript-eslint/strict-boolean-expressions": "error",
       "@typescript-eslint/switch-exhaustiveness-check": "error",
     },
