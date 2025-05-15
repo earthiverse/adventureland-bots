3248a648dbde788f570f447e83df452ff269e25e
Kent Rasmussen <hyprkookeez@gmail.com>
2025-05-15 11:36:27 +0900

    add rogue stoneworm strategy

--- source/strategy_pattern/setups/stoneworm.ts
+++ source/strategy_pattern/setups/stoneworm.ts
@@ -7,6 +7,7 @@ import { ImprovedMoveStrategy, KiteMonsterMoveStrategy } from "../strategies/mov
 import { Requirements, Setup } from "./base.js"
 import { MAGE_SPLASH, PRIEST_LUCK, WARRIOR_SPLASH } from "./equipment.js"
 import { RangerAttackStrategy } from "../strategies/attack_ranger.js"
+import { RogueAttackStrategy } from "../strategies/attack_rogue.js"
 
 export function constructStoneWormSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
     const moveStrategy = new ImprovedMoveStrategy("stoneworm")
@@ -22,11 +23,11 @@ export function constructStoneWormSetup(contexts: Strategist<PingCompensatedChar
                             contexts: contexts,
                             disableEnergize: true,
                             generateEnsureEquipped: {
-                                prefer: { ...MAGE_SPLASH }
+                                prefer: { ...MAGE_SPLASH },
                             },
-                            type: "stoneworm"
+                            type: "stoneworm",
                         }),
-                        move: moveStrategy
+                        move: moveStrategy,
                     },
                     {
                         ctype: "priest",
@@ -34,11 +35,11 @@ export function constructStoneWormSetup(contexts: Strategist<PingCompensatedChar
                             contexts: contexts,
                             disableEnergize: true,
                             generateEnsureEquipped: {
-                                prefer: { ...PRIEST_LUCK }
+                                prefer: { ...PRIEST_LUCK },
                             },
                             type: "stoneworm",
                         }),
-                        move: moveStrategy
+                        move: moveStrategy,
                     },
                     {
                         ctype: "warrior",
@@ -47,24 +48,45 @@ export function constructStoneWormSetup(contexts: Strategist<PingCompensatedChar
                             enableEquipForCleave: true,
                             enableGreedyAggro: true,
                             generateEnsureEquipped: {
-                                prefer: { ...WARRIOR_SPLASH }
+                                prefer: { ...WARRIOR_SPLASH },
                             },
-                            type: "stoneworm"
+                            type: "stoneworm",
                         }),
-                        move: moveStrategy
-                    }
-                ]
+                        move: moveStrategy,
+                    },
+                ],
             },
-        ]
+            {
+                id: "stoneworm_rogue",
+                characters: [
+                    {
+                        ctype: "rogue",
+                        attack: new RogueAttackStrategy({
+                            contexts: contexts,
+                            generateEnsureEquipped: { attributes: ["range"] },
+                        }),
+                        move: new KiteMonsterMoveStrategy({
+                            contexts: contexts,
+                            disableCheckDB: true,
+                            typeList: ["stoneworm"],
+                        }),
+                    },
+                ],
+            },
+        ],
     }
 }
 
 export function constructStoneWormHelperSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
-    const moveStrategy = new KiteMonsterMoveStrategy({ contexts: contexts, disableCheckDB: true, typeList: ["stoneworm"] })
+    const moveStrategy = new KiteMonsterMoveStrategy({
+        contexts: contexts,
+        disableCheckDB: true,
+        typeList: ["stoneworm"],
+    })
     const requirements: Requirements = {
         items: ["jacko"],
         range: AL.Game.G.monsters.stoneworm.range + 50,
-        speed: AL.Game.G.monsters.stoneworm.charge
+        speed: AL.Game.G.monsters.stoneworm.charge,
     }
 
     return {
@@ -77,12 +99,12 @@ export function constructStoneWormHelperSetup(contexts: Strategist<PingCompensat
                         attack: new MageAttackStrategy({
                             contexts: contexts,
                             disableEnergize: true,
-                            type: "stoneworm"
+                            type: "stoneworm",
                         }),
                         move: moveStrategy,
-                        require: requirements
-                    }
-                ]
+                        require: requirements,
+                    },
+                ],
             },
             {
                 id: "stoneworm_ranger",
@@ -92,13 +114,13 @@ export function constructStoneWormHelperSetup(contexts: Strategist<PingCompensat
                         attack: new RangerAttackStrategy({
                             contexts: contexts,
                             disableEnergize: true,
-                            type: "stoneworm"
+                            type: "stoneworm",
                         }),
                         move: moveStrategy,
-                        require: requirements
-                    }
-                ]
+                        require: requirements,
+                    },
+                ],
             },
-        ]
+        ],
     }
 }
