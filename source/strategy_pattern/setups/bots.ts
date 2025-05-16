b6e42b5eff7a382bfb77e6d4cd316a04eb3045c8
Kent Rasmussen <hyprkookeez@gmail.com>
2025-05-16 13:04:58 +0900

    prefer xp

--- source/strategy_pattern/setups/bots.ts
+++ source/strategy_pattern/setups/bots.ts
@@ -24,6 +24,7 @@ export function constructBotsSetup(contexts: Strategist<PingCompensatedCharacter
         attack: new RogueAttackStrategy({
             contexts: contexts,
             generateEnsureEquipped: {
+                attributes: ["xp"],
                 prefer: {
                     mainhand: { name: "cclaw", filters: RETURN_HIGHEST },
                     offhand: { name: "cclaw", filters: RETURN_HIGHEST },
