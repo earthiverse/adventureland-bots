3751b5fab5dc5beb4c22d1ab24df16c131e4e87a
Kent Rasmussen <hyprkookeez@gmail.com>
2025-05-16 13:34:40 +0900

    improve avoid stacking strategy

--- source/strategy_pattern/strategies/avoid_stacking.ts
+++ source/strategy_pattern/strategies/avoid_stacking.ts
@@ -1,20 +1,28 @@
 import { Character, HitData } from "alclient"
-import { /* Loop, LoopName, */ Strategy } from "../context.js"
+import { Strategy } from "../context.js"
+import { suppress_errors } from "../logging.js"
 
 export class AvoidStackingStrategy<Type extends Character> implements Strategy<Type> {
     // public loops = new Map<LoopName, Loop<Type>>()
     private onHit: (data: HitData) => Promise<void>
 
+    private lastMove = new Map<Type, number>()
+
     public constructor() {
         // this.loops.set("avoid_stacking", {
-        //     fn: async (bot: Type) => { await this.checkStacking(bot) },
-        //     interval: 250
+        //     fn: async (bot: Type) => {
+        //         await this.checkStacking(bot)
+        //     },
+        //     interval: 250,
         // })
     }
 
     // private async checkStacking(bot: Type) {
     //     if (bot.moving || bot.smartMoving) return // We're moving, don't check stacking
 
+    //     for(const other of bot.getPlayers({ withinRange: 0 })) {
+
+    //     }
     //     // TODO: Check if we're on top of another player. If we are, find an empty spot to move to.
     // }
 
@@ -22,13 +30,18 @@ export class AvoidStackingStrategy<Type extends Character> implements Strategy<T
         this.onHit = async (data: HitData) => {
             if (data.id !== bot.id) return // Not for us
             if (!data.stacked) return
+            if (bot.moving || bot.smartMoving) return // We're moving, don't check stacking
+            if (this.lastMove.has(bot) && this.lastMove.get(bot) >= Date.now() - 250) return // We recently moved
             if (!data.stacked.includes(bot.id)) return // We're not the ones that are stacked
 
             console.info(`Moving ${bot.id} to avoid stacking!`)
 
             const x = -25 + Math.round(50 * Math.random())
             const y = -25 + Math.round(50 * Math.random())
-            await bot.move(bot.x + x, bot.y + y).catch(() => { /* Suppress errors */ })
+
+            this.lastMove.set(bot, Date.now())
+
+            await bot.move(bot.x + x, bot.y + y).catch(suppress_errors)
         }
 
         bot.socket.on("hit", this.onHit)
