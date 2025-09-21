7a203639d3535ee4e4647bc8892eabf967667db8
Kent Rasmussen <hyprkookeez@gmail.com>
2025-09-21 15:59:02 +0900

    add priest/ranger dryad strategy

--- source/strategy_pattern/setups/dryad.ts
+++ source/strategy_pattern/setups/dryad.ts
@@ -4,11 +4,34 @@ import { MageAttackStrategy } from "../strategies/attack_mage.js"
 import { PriestAttackStrategy } from "../strategies/attack_priest.js"
 import { WarriorAttackStrategy } from "../strategies/attack_warrior.js"
 import { ImprovedMoveStrategy } from "../strategies/move.js"
-import { Setup } from "./base"
-import { JACKO, MAGE_SPLASH_WEAPONS, SUPERMITTENS, WARRIOR_SPLASH_WEAPONS, ZAPPER_CRING } from "./equipment.js"
+import { CharacterConfig, Setup } from "./base"
+import {
+    JACKO,
+    MAGE_SPLASH_WEAPONS,
+    RANGER_SPLASH_WEAPONS,
+    SUPERMITTENS,
+    WARRIOR_SPLASH_WEAPONS,
+    ZAPPER_CRING,
+} from "./equipment.js"
+import { RangerAttackStrategy } from "../strategies/attack_ranger.js"
 
 export function constructDryadSetup(contexts: Strategist<PingCompensatedCharacter>[]): Setup {
     const moveStrategy = new ImprovedMoveStrategy("dryad")
+
+    const priestStrategy: CharacterConfig = {
+        ctype: "priest",
+        attack: new PriestAttackStrategy({
+            contexts: contexts,
+            enableAbsorbToTank: true,
+            enableGreedyAggro: true,
+            generateEnsureEquipped: {
+                attributes: ["resistance", "int", "attack"],
+                prefer: { ...ZAPPER_CRING },
+            },
+            type: "dryad",
+        }),
+        move: moveStrategy,
+    }
 
     return {
         configs: [
@@ -27,19 +50,7 @@ export function constructDryadSetup(contexts: Strategist<PingCompensatedCharacte
                         }),
                         move: moveStrategy,
                     },
-                    {
-                        ctype: "priest",
-                        attack: new PriestAttackStrategy({
-                            contexts: contexts,
-                            enableGreedyAggro: true,
-                            generateEnsureEquipped: {
-                                attributes: ["resistance", "int", "attack"],
-                                prefer: { ...ZAPPER_CRING },
-                            },
-                            type: "dryad",
-                        }),
-                        move: moveStrategy,
-                    },
+                    priestStrategy,
                     {
                         ctype: "warrior",
                         attack: new WarriorAttackStrategy({
@@ -50,6 +61,26 @@ export function constructDryadSetup(contexts: Strategist<PingCompensatedCharacte
                             generateEnsureEquipped: {
                                 attributes: ["resistance", "str", "attack"],
                                 prefer: { ...WARRIOR_SPLASH_WEAPONS, ...SUPERMITTENS },
+                            },
+                            type: "dryad",
+                        }),
+                        move: moveStrategy,
+                    },
+                ],
+            },
+            {
+                id: "dryad_priest,ranger",
+                characters: [
+                    priestStrategy,
+                    {
+                        ctype: "ranger",
+                        attack: new RangerAttackStrategy({
+                            contexts: contexts,
+                            generateEnsureEquipped: {
+                                attributes: ["resistance", "blast", "explosion"],
+                                prefer: {
+                                    ...RANGER_SPLASH_WEAPONS,
+                                },
                             },
                             type: "dryad",
                         }),
