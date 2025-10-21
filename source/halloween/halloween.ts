c0217d402526adcc0d5e9a7c3174bd2c7e77cf58
Kent Rasmussen <hyprkookeez@gmail.com>
2025-10-21 20:31:38 +0900

    tweak server priority

--- source/halloween/halloween.ts
+++ source/halloween/halloween.ts
@@ -127,7 +127,7 @@ let currentRegion: ServerRegion = "US"
 let currentIdentifier: ServerIdentifier = "HARDCORE" // Start on a non-existent server, the first loop will set this to something else
 
 type Server = `${ServerRegion}${ServerIdentifier}`
-const SERVER_PRIORITY: Server[] = ["USI", "EUI", "USII", "USIII", "EUII", "ASIAI", "USPVP", "EUPVP"]
+const SERVER_PRIORITY: Server[] = ["EUI", "USIII", "EUII", "USI", "USII", "ASIAI", "USPVP", "EUPVP"]
 
 const activeStrategists: Strategist<PingCompensatedCharacter>[] = []
 
