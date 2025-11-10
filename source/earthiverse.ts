6b99791c78c69ff13a8780eb4028e44fce581d34
Kent Rasmussen <hyprkookeez@gmail.com>
2025-11-10 09:55:08 +0900

    bots get stuck on harpies without cheats

--- source/earthiverse.ts
+++ source/earthiverse.ts
@@ -80,7 +80,7 @@ import { GiveRogueSpeedStrategy } from "./strategy_pattern/strategies/rspeed.js"
 import { TrackUpgradeStrategy } from "./strategy_pattern/strategies/statistics.js"
 
 await Promise.all([AL.Game.loginJSONFile("../credentials.json", false), AL.Game.getGData(true)])
-await AL.Pathfinder.prepare(AL.Game.G, { remove_abtesting: true, remove_test: true })
+await AL.Pathfinder.prepare(AL.Game.G, { cheat: true, remove_abtesting: true, remove_test: true })
 
 // TODO: Make these configurable through /comm
 // Toggles
