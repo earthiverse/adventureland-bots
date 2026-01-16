2a848888ed05337bf8b9d9bdb97b6ef85bec52de
Kent Rasmussen <hyprkookeez@gmail.com>
2026-01-16 14:13:51 +0900

    test fix for buggy equip batch

--- source/strategy_pattern/strategies/attack.ts
+++ source/strategy_pattern/strategies/attack.ts
@@ -508,11 +508,7 @@ export class BaseAttackStrategy<Type extends Character> implements Strategy<Type
             }
         }
 
-        if (equipBatch.length) {
-            console.log("Equipping batch for ensureEquipped")
-            console.log(equipBatch)
-            await bot.equipBatch(equipBatch).catch(console.error)
-        }
+        if (equipBatch.length) await bot.equipBatch(equipBatch).catch(console.error)
     }
 
     protected async scare(bot: Type) {
