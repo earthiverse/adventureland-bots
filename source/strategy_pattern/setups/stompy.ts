13f7ba76b1c09331df13971d1bbf7ac12f1486d7
Kent Rasmussen <hyprkookeez@gmail.com>
2026-01-20 12:08:32 +0900

    lower stompy hp to switch

--- source/strategy_pattern/setups/stompy.ts
+++ source/strategy_pattern/setups/stompy.ts
@@ -64,7 +64,7 @@ export function constructStompySetup(contexts: Strategist<PingCompensatedCharact
                             switchConfig: [
                                 [
                                     "stompy",
-                                    1_000_000,
+                                    200_000,
                                     {
                                         attributes: ["luck"],
                                         prefer: {
