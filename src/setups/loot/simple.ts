ca0b2aff8b9e42076e60ae7ecb8e17ed5efa0c7b
Kent Rasmussen <hyprkookeez@gmail.com>
2026-02-05 21:23:20 +0900

    improve loot logic

--- src/setups/loot/simple.ts
+++ src/setups/loot/simple.ts
@@ -1,3 +1,4 @@
+import { TTLCache } from "@isaacs/ttlcache";
 import type { Character } from "alclient";
 import type { ServerToClient_drop } from "typed-adventureland";
 import { logDebug } from "../../utilities/logging.js";
@@ -7,7 +8,7 @@ type ActiveData = {
 };
 const active = new Map<Character, ActiveData>();
 
-// TODO: Add active characters, and check if they are the best looter
+const recentlyLooted = new TTLCache<string, ServerToClient_drop>({ ttl: 1000 });
 
 /**
  * Starts the loot logic for the given character
@@ -22,7 +23,10 @@ export const setup = (character: Character) => {
   const dropHandler = async (data: ServerToClient_drop) => {
     try {
       if (activeData.cancelled) character.socket.off("drop", dropHandler);
+      if (recentlyLooted.has(data.id)) return; // Already looted
+      if (!isBestLooter(character, data)) return; // Not the best looter
 
+      recentlyLooted.set(data.id, data);
       await character.openChest(data.id);
     } catch (e) {
       if (e instanceof Error || typeof e === "string") logDebug(`dropHandler: ${e}`);
@@ -30,7 +34,28 @@ export const setup = (character: Character) => {
   };
   character.socket.on("drop", dropHandler);
 
-  // TODO: loot loop if there are errors looting
+  const lootLoop = async () => {
+    if (activeData.cancelled) return;
+
+    try {
+      if (character.socket.disconnected) return;
+      if (character.rip) return;
+
+      for (const chest of character.chests.values()) {
+        if (recentlyLooted.has(chest.id)) continue; // Already looted
+        if (!isBestLooter(character, chest)) continue; // Not the best
+
+        recentlyLooted.set(chest.id, chest);
+        await character.openChest(chest.id);
+        break; // Only loot one chest per loop
+      }
+    } catch (e) {
+      if (e instanceof Error || typeof e === "string") logDebug(`lootLoop: ${e}`);
+    } finally {
+      setTimeout(() => void lootLoop(), 100);
+    }
+  };
+  void lootLoop();
 };
 
 /**
@@ -40,4 +65,23 @@ export const setup = (character: Character) => {
 export function teardown(character: Character) {
   if (active.has(character)) active.get(character)!.cancelled = true;
   active.delete(character);
+}
+
+function isBestLooter(character: Character, chest: ServerToClient_drop) {
+  let bestGoldM = Number.NEGATIVE_INFINITY;
+  let bestLooters: Character[] = [];
+
+  for (const looter of active.keys()) {
+    const distanceToChest = character.getDistanceTo({ map: chest.map, in: character.in, x: chest.x, y: chest.y });
+    const goldM = distanceToChest > 400 ? 1 : looter.goldm;
+
+    if (goldM > bestGoldM) {
+      bestGoldM = goldM;
+      bestLooters = [looter];
+    } else if (goldM === bestGoldM) {
+      bestLooters.push(looter);
+    }
+  }
+
+  return bestLooters.includes(character);
 }
