6dd0db026c8fd9737d722d164c5d6c58aa1f39b1
Kent Rasmussen <hyprkookeez@gmail.com>
2026-02-08 13:32:11 +0900

    tweak positions

--- src/utilities/positions.ts
+++ src/utilities/positions.ts
@@ -1,5 +1,5 @@
 import type { IPosition } from "typed-adventureland";
 
-export const BANK_CENTER: IPosition = { map: "bank", in: "bank", x: 0, y: -250 };
-export const BANK_BASEMENT_CENTER: IPosition = { map: "bank_b", in: "bank_b", x: -275, y: -200 };
-export const BANK_UNDERGROUND_CENTER: IPosition = { map: "bank_u", in: "bank_u", x: -140, y: 200 };
+export const BANK_CENTER: Required<IPosition> = { map: "bank", in: "bank", x: 0, y: -250 };
+export const BANK_BASEMENT_CENTER: Required<IPosition> = { map: "bank_b", in: "bank_b", x: -275, y: -200 };
+export const BANK_UNDERGROUND_CENTER: Required<IPosition> = { map: "bank_u", in: "bank_u", x: -140, y: 200 };
