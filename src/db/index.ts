a4c460a1183614dfc36143c6390eab9ddc631484
Kent Rasmussen <kent@gplusmedia.com>
2026-08-21 12:44:24 +0900

    improve db concurrency

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,4 +7,8 @@ import * as schema from "./schema.js";
 const dbPath = config.db.path === ":memory:" ? ":memory:" : path.resolve(process.cwd(), config.db.path);
 const client = new Database(dbPath);
 
+// Allow multiple processes to use the same DB
+client.run("PRAGMA journal_mode = WAL;");
+client.run("PRAGMA busy_timeout = 100;");
+
 export const db = drizzle(client, { schema });
